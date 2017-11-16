5c27c1116d87020df076c3a62d4109ce5256774b
Zhou Wang <zhouwang@microsoft.com>
2017-11-16 13:05:20 +0100

    add uspmessage.h and usp.h

---
+++ source/core/include/usp.h
@@ -0,0 +1,111 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+// usp.h: the header for usplib
+//
+
+#pragma once
+
+#include <stdint.h>
+
+#include "uspmessages.h"
+
+/**
+ * The UspHandle represents an opaque handle used by usplib.
+ */
+typedef void * UspHandle;
+
+/**
+ * The USP_ERROR represents error codes.
+ */
+typedef unsigned int USP_ERROR;
+
+#define USP_SUCCESS ((USP_ERROR)0)
+
+#define USP_ERRCODE(x) (0x800f6000 | (x & 0x0fff))
+
+
+/**
+ * The PUspOnSpeechStartDetectedCallback represents an application-defined callback function
+ * used for signaling a speech.startDetected message.
+ * @param handle The UspHandle.
+ * @param context A pointer to the application-defined callback context.
+ * @param message A pointer to the speech.startDetected message.
+*/
+typedef void(*PUspOnSpeechStartDetectedCallback)(UspHandle handle, void* context, UspMsgSpeechStartDetected *message);
+
+/**
+* The PUspOnSpeechEndDetectedCallback represents an application-defined callback function
+* used for signaling a speech.endDetected message.
+* @param handle The UspHandle.
+* @param context A pointer to the application-defined callback context.
+* @param message A pointer to the speech.endDetected message.
+*/
+typedef void(*PUspOnSpeechEndDetectedCallback)(UspHandle handle, void* context, UspMsgSpeechEndDetected *message);
+
+/**
+* The PUspOnSpeechHypothesisCallback represents an application-defined callback function
+* used for signaling a speech.hypothesis message.
+* @param handle The UspHandle.
+* @param context A pointer to the application-defined callback context.
+* @param message A pointer to the speech.hypothesis message.
+*/
+typedef void(*PUspOnSpeechHypothesisCallback)(UspHandle handle, void* context, UspMsgSpeechHypothesis *message);
+
+/**
+* The PUspOnSpeechPhraseCallback represents an application-defined callback function
+* used for signaling a speech.phrase message.
+* @param handle The UspHandle.
+* @param context A pointer to the application-defined callback context.
+* @param message A pointer to the speech.phrase message.
+*/
+typedef void(*PUspOnSpeechPhraseCallback)(UspHandle handle, void* context, UspMsgSpeechPhrase *message);
+
+/**
+* The PUspOnTurnStartCallback represents an application-defined callback function
+* used for signaling a turn.start message.
+* @param handle The UspHandle.
+* @param context A pointer to the application-defined callback context.
+* @param message A pointer to the turn.start message.
+*/
+typedef void(*PUspOnTurnStartCallback)(UspHandle handle, void* context, UspMsgTurnStart *message);
+
+/**
+* The PUspOnTurnEndDetectedCallback represents an application-defined callback function
+* used for signaling a turn.end message.
+* @param handle The UspHandle.
+* @param context A pointer to the application-defined callback context.
+* @param message A pointer to the turn.end message.
+*/
+typedef void(*PUspOnTurnEndCallback)(UspHandle handle, void* context, UspMsgTurnEnd *message);
+
+/**
+* The PUspOnErrorCallback represents an application-defined callback function
+* used for signaling the an error.
+* @param handle The UspHandle.
+* @param context A pointer to the application-defined callback context.
+* @param error an error code.
+*/
+typedef void(*PUspOnErrorCallback)(UspHandle handle, void* context, USP_ERROR error);
+
+
+/**
+* The UspCallbacks type represents an application-defined
+* structure used to register all various usp events.
+*/
+typedef struct _UspCallbacks
+{
+    uint16_t size;
+    uint16_t version;
+    PUspOnSpeechStartDetectedCallback onSpeechStartDetected;
+    PUspOnSpeechEndDetectedCallback onSpeechEndDetected;
+    PUspOnSpeechHypothesisCallback onSpeechHypothesis;
+    PUspOnSpeechPhraseCallback onSpeechPhrase;
+    PUspOnTurnStartCallback onTurnStart;
+    PUspOnTurnEndCallback onTurnEnd;
+    PUspOnErrorCallback OnError;
+} UspCallbacks;
+
+
+int UspInitialize(UspHandle* ppHandle, UspCallbacks *pCallbacks, void* pContext);
---
+++ source/core/include/uspmessages.h
@@ -0,0 +1,77 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+// usp.h: the header for usplib
+//
+#pragma once
+
+#include <stdint.h>
+
+typedef uint32_t UspOffsetType;
+typedef uint32_t UspDurationType;
+
+/**
+ * Represents recognition status in speech phrase.
+ */
+typedef enum _UspRecognitionStatus
+{
+    USP_RECOGNITON_SUCCESS,
+    USP_RECOGNITION_NO_MATCH,
+    USP_RECOGNITION_INITIAL_SILENCE_TIMEOUT,
+    USP_RECOGNITION_BABBLE_TIMEOUT,
+    USP_RECOGNITION_ERROR
+} UspRecognitionStatus;
+
+/**
+ * Represents speech.startDectected message
+ */
+typedef struct _UspMsgSpeechStartDetected
+{
+    UspOffsetType Offset;
+} UspMsgSpeechStartDetected;
+
+/**
+ * Represents speech.hypothesis message
+ */
+typedef struct _UspMsgSpeechHypothesis
+{
+    wchar_t* Text;
+    UspOffsetType Offset;
+    UspDurationType Duration;
+} UspMsgSpeechHypothesis;
+
+/**
+ * Represents speech.phrase message
+ */
+typedef struct _UspMsgSpeechPhrase
+{
+    UspRecognitionStatus RecognitionStatus;
+    wchar_t* DisplayText;
+    UspOffsetType Offset;
+    UspDurationType Duration;
+} UspMsgSpeechPhrase;
+
+/**
+ * Represents speech.endDetected message
+ */
+typedef struct _UspMsgSpeechEndDetected
+{
+    UspOffsetType Offset;
+} UspMsgSpeechEndDetected;
+
+/**
+ * Represnets turn.start message
+ */
+typedef struct _UspMsgTurnStart
+{
+    wchar_t* ContextServiceTag;
+} UspMsgTurnStart;
+
+/**
+ * Represents turn.end message
+ */
+typedef struct _UspMsgTurnEnd
+{
+    // body is empty.
+} UspMsgTurnEnd;
