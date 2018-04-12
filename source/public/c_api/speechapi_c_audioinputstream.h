3919c0fd633b076f612a303977b2b2f5fc3c861f
Friedel van Megen <Friedrich.van.Megen@microsoft.com>
2018-04-12 22:16:10 +0000

    Merged PR 595724: adding doxygen docs for the audio input stream
    adding doxygen docs for the audio input stream
    
    Related work items: #1197011

--- source/public/c_api/speechapi_c_audioinputstream.h
+++ source/public/c_api/speechapi_c_audioinputstream.h
@@ -2,33 +2,98 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
-// speechapi_c_session.h: Public API declarations for AudioInputStream related C methods
+// speechapi_c_audioinputstream.h: Public API declarations for AudioInputStream related C methods
 //
 
 #pragma once
 #include <speechapi_c_common.h>
 
+/// <summary>
+/// A structure that describes the audio format.
+/// </summary>
 typedef struct tAudioInputStreamFormatC
 {
-    unsigned short wFormatTag;         /* format type */
-    unsigned short nChannels;          /* number of channels (i.e. mono, stereo...) */
-    unsigned int   nSamplesPerSec;     /* sample rate */
-    unsigned int   nAvgBytesPerSec;    /* for buffer estimation */
-    unsigned short nBlockAlign;        /* block size of data */
-    unsigned short wBitsPerSample;     /* number of bits per sample of mono data */
-    unsigned short cbSize;             /* the count in bytes of the size of */
-                                       /* extra information (after cbSize) */
+    /// <summary>
+    /// The format of the audio, valid values: 1 (PCM)
+    /// </summary>
+    unsigned short wFormatTag;
+
+    /// <summary>
+    /// The number of channels, valid values: 1 (Mono).
+    /// </summary>
+    unsigned short nChannels;
+
+    /// <summary>
+    /// The sample rate, valid values: 16000.
+    /// </summary>
+    unsigned int   nSamplesPerSec;
+
+    /// <summary>
+    /// Average bytes per second, usually calculated as nSamplesPerSec * nChannels * ceil(wBitsPerSample, 8).
+    /// </summary>
+    unsigned int   nAvgBytesPerSec;
+
+    /// <summary>
+    /// The size of a single frame, valid values: nChannels * ceil(wBitsPerSample, 8).
+    /// </summary>
+    unsigned short nBlockAlign;
+
+    /// <summary>
+    /// The bits per sample, valid values: 16
+    /// </summary>
+    unsigned short wBitsPerSample;
+
+    /// <summary>
+    /// The size of this structure, valid values: sizeof(AudioInputStreamFormat).
+    /// </summary>
+    unsigned short cbSize;
 } AudioInputStreamFormat, *PAudioInputStreamFormat;
 
+// forward declaration
 struct _AudioInputStream;
 
+/// <summary>
+/// Signature of a function being called to get the format of the audio stream.
+/// </summary>
+/// <param name="context">The pointer to the struct _AudioInputStream for which the parameter is queried.</param>
+/// <param name="pformat">The pointer to the struct tAudioInputStreamFormatC buffer, or null if querying the size of the structure.</param>
+/// <param name="cbFormat">The size of the AudioInputStreamFormat buffer being passed, or 0 if querying the size of the structure.</param>
+/// <returns>The size of the AudioInputStreamFormat buffer required to hold the format information.</returns>
 typedef unsigned short (*PAUDIOINPUTSTREAM_GETFORMAT_CALLBACK_FUNC)(struct _AudioInputStream *context, struct tAudioInputStreamFormatC* pformat, unsigned short cbFormat);
+
+/// <summary>
+/// Signature of a function being called to get the data from the audio stream.
+/// </summary>
+/// <param name="context">The pointer to the struct _AudioInputStream for which the data is queried.</param>
+/// <param name="pbuffer">The pointer to the buffer to which to copy the audio data.</param>
+/// <param name="cbBuffer">The size of the buffer.</param>
+/// <returns>The number of bytes being copied to the buffer.</returns>
 typedef unsigned int (*PAUDIOINPUTSTREAM_READ_CALLBACK_FUNC)(struct _AudioInputStream *context, unsigned char* pbuffer, unsigned int cbBuffer);
+
+/// <summary>
+/// Signature of a function being called to close the audio stream.
+/// </summary>
+/// <param name="context">The pointer to the struct _AudioInputStream to close.</param>
 typedef void(*PAUDIOINPUTSTREAM_CLOSE_CALLBACK_FUNC)(struct _AudioInputStream *context);
 
+
+/// <summary>
+/// A structure with function pointers for the 3 operations defined on an AudioInputStream.
+/// </summary>
 typedef struct _AudioInputStream
 {
+    /// <summary>
+    /// The Function being called to get the format of the audio data.
+    /// </summary>
     PAUDIOINPUTSTREAM_GETFORMAT_CALLBACK_FUNC GetFormat;
+
+    /// <summary>
+    /// The Function being called to get the data of the audio stream.
+    /// </summary>
     PAUDIOINPUTSTREAM_READ_CALLBACK_FUNC Read;
+
+    /// <summary>
+    /// The Function being called to close the audio stream.
+    /// </summary>
     PAUDIOINPUTSTREAM_CLOSE_CALLBACK_FUNC Close;
 } SpeechApi_AudioInputStream;
--- source/public/cxx_api/speechapi_cxx_audioinputstream.h
+++ source/public/cxx_api/speechapi_cxx_audioinputstream.h
@@ -2,7 +2,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
-// speechapi_cxx_AudioInputStream.h: Public API declarations for AudioInputStream C++ base class
+// speechapi_cxx_audioinputstream.h: Public API declarations for AudioInputStream C++ base class
 //
 
 #pragma once
@@ -12,38 +12,94 @@
 
 namespace CARBON_NAMESPACE_ROOT {
 
+/// <summary>
+/// A structure that describes the audio format.
+/// </summary>
 typedef struct tAudioInputStreamFormat
 {
-    unsigned short wFormatTag;         /* format type */
-    unsigned short nChannels;          /* number of channels (i.e. mono, stereo...) */
-    unsigned int   nSamplesPerSec;     /* sample rate */
-    unsigned int   nAvgBytesPerSec;    /* for buffer estimation */
-    unsigned short nBlockAlign;        /* block size of data */
-    unsigned short wBitsPerSample;     /* number of bits per sample of mono data */
-    unsigned short cbSize;             /* the count in bytes of the size of */
-                                       /* extra information (after cbSize) */
+    /// <summary>
+    /// The format of the audio, valid values: 1 (PCM)
+    /// </summary>
+    unsigned short wFormatTag;
+
+    /// <summary>
+    /// The number of channels, valid values: 1 (Mono).
+    /// </summary>
+    unsigned short nChannels;
+
+    /// <summary>
+    /// The sample rate, valid values: 16000.
+    /// </summary>
+    unsigned int   nSamplesPerSec;
+
+    /// <summary>
+    /// Average bytes per second, usually calculated as nSamplesPerSec * nChannels * ceil(wBitsPerSample, 8).
+    /// </summary>
+    unsigned int   nAvgBytesPerSec;
+
+    /// <summary>
+    /// The size of a single frame, valid values: nChannels * ceil(wBitsPerSample, 8).
+    /// </summary>
+    unsigned short nBlockAlign;
+
+    /// <summary>
+    /// The bits per sample, valid values: 16
+    /// </summary>
+    unsigned short wBitsPerSample;
+
+    /// <summary>
+    /// The size of this structure. valid values: sizeof(AudioInputStreamFormat).
+    /// </summary>
+    unsigned short cbSize;
 } AudioInputStreamFormat, *PAudioInputStreamFormat;
 
+
+/// <summary>
+/// A Interface that defines an AudioInputStream.
+/// </summary>
+/// <remarks>
+/// Derive from this class and implement its function to provide your own
+/// data as an Audio input stream.
+/// <remarks>
 class AudioInputStream
 {
 public:
-
+    /// <summary>
+    /// Destructor, does nothing.
+    /// </summary>
     virtual ~AudioInputStream() {}
 
+    /// <summary>
+    /// The Function being called to get the format of the audio stream.
+    /// </summary>
+    /// <param name="pformat">The pointer to the AudioInputStreamFormat buffer, or null if querying the size of the structure.</param>
+    /// <param name="cbFormat">The size of the AudioInputStreamFormat buffer being passed, or 0 if querying the size of the structure.</param>
+    /// <returns>The size of the AudioInputStreamFormat buffer required to hold the format information.</returns>
     virtual unsigned short GetFormat(AudioInputStreamFormat* pformat, unsigned short cbFormat) = 0;
+
+    /// <summary>
+    /// The Function being called to get the data from the audio stream.
+    /// </summary>
+    /// <param name="dataBuffer">The pointer to the buffer to which to copy the audio data.</param>
+    /// <param name="size">The size of the buffer.</param>
+    /// <returns>The number of bytes being copied to the buffer.</returns>
     virtual unsigned int Read(char* dataBuffer, unsigned int size) = 0;
+
+    /// <summary>
+    /// The Function being called to close the audio stream.
+    /// </summary>
     virtual void Close() = 0;
 
 protected:
-
+    /// <summary>
+    /// Constructor, does nothing.
+    /// </summary>
     AudioInputStream() {};
 
 
 private:
-
     AudioInputStream(const AudioInputStream&) = delete;
     AudioInputStream(const AudioInputStream&&) = delete;
-
     AudioInputStream& operator=(const AudioInputStream&) = delete;
 };
 
