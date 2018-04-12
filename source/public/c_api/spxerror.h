1dcce2ffef9017ee785cf7ce5c5466c3268c3442
Mark Hillebrand <mahilleb@microsoft.com>
2018-04-12 16:41:59 +0000

    Merged PR 595368: Convert docs for c_api/spxerror.h
    Convert docs for c_api/spxerror.h
    
    Related work items: #1134456

--- source/public/c_api/spxerror.h
+++ source/public/c_api/spxerror.h
@@ -2,154 +2,179 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
-// spxerror.h: Public API definitions for global SPX errors and realted #define macros
-//
+/// \file spxerror.h
+/// \brief Public API definitions for global SPX errors and related #define macros.
 
 #pragma once
 
+/// <summary>
+/// Type definition for SPX result codes.
+/// </summary>
 typedef unsigned int SPXHR;
 
+/// <summary>
+/// Default result code indicating no error.
+/// </summary>
 #define SPX_NOERROR                 0x00000000
+
+/// <summary>
+/// Declare and initialize result code variable.
+/// </summary>
 #define SPX_INIT_HR(hr)             SPXHR hr = SPX_NOERROR; \
                                     (void)(hr)
 
-#define SPX_SUCCEEDED(x)            ((x & 0x80000000) == 0)
-#define SPX_FAILED(x)               (!SPX_SUCCEEDED(x))   
+/// <summary>
+/// Check if result code indicates success.
+/// </summary>
+#define SPX_SUCCEEDED(x)            (((x) & 0x80000000) == 0)
 
-#define __SPX_ERRCODE_SUCCEEDED(x)  (0x000f5000 | (x & 0x0fff))
-#define __SPX_ERRCODE_FAILED(x)     (0x800f5000 | (x & 0x0fff))
+/// <summary>
+/// Check if result code indicates error.
+/// </summary>
+#define SPX_FAILED(x)               (!SPX_SUCCEEDED(x))
 
+#define __SPX_ERRCODE_SUCCEEDED(x)  (0x000f5000 | ((x) & 0x0fff))
+#define __SPX_ERRCODE_FAILED(x)     (0x800f5000 | ((x) & 0x0fff))
 
-/*** SPXERR_NOT_IMPL
-*   The function is not implemented.
-*/
+/// <summary>
+/// The function is not implemented.
+/// </summary>
 #define SPXERR_NOT_IMPL             __SPX_ERRCODE_FAILED(0xfff)
 
-/*** SPXERR_UNINITIALIZED
-*   The object has not been properly initialized.
-*/
+/// <summary>
+/// The object has not been properly initialized.
+/// </summary>
 #define SPXERR_UNINITIALIZED        __SPX_ERRCODE_FAILED(0x001)
 
-/*** SPXERR_ALREADY_INITIALIZED
-*   The object has already been initialized.
-*/
+/// <summary>
+/// The object has already been initialized.
+/// </summary>
 #define SPXERR_ALREADY_INITIALIZED  __SPX_ERRCODE_FAILED(0x002)
 
-/*** SPXERR_UNHANDLED_EXCEPTION
-*   An unnhandled exception was detected.
-*/
+/// <summary>
+/// An unhandled exception was detected.
+/// </summary>
 #define SPXERR_UNHANDLED_EXCEPTION  __SPX_ERRCODE_FAILED(0x003)
 
-/*** SPXERR_NOT_FOUND
-*   The object or property was not found.
-*/
+/// <summary>
+/// The object or property was not found.
+/// </summary>
 #define SPXERR_NOT_FOUND            __SPX_ERRCODE_FAILED(0x004)
 
-/*** SPXERR_INVALID_ARG
-*   One or more arguments are not valid.
-*/
+/// <summary>
+/// One or more arguments are not valid.
+/// </summary>
 #define SPXERR_INVALID_ARG          __SPX_ERRCODE_FAILED(0x005)
 
-/*** SPXERR_TIMEOUT
-*   The specified timeout value has ellapsed.
-*/
+/// <summary>
+/// The specified timeout value has elapsed.
+/// </summary>
 #define SPXERR_TIMEOUT              __SPX_ERRCODE_FAILED(0x006)
 
-/*** SPXERR_ALREADY_IN_PROGRESS
-*   The asynchronous operation is already in progress.
-*/
+/// <summary>
+/// The asynchronous operation is already in progress.
+/// </summary>
 #define SPXERR_ALREADY_IN_PROGRESS  __SPX_ERRCODE_FAILED(0x007)
 
-/*** SPXERR_FILE_OPEN_FAILED
-*   The attempt to open the file failed.
-*/
+/// <summary>
+/// The attempt to open the file failed.
+/// </summary>
 #define SPXERR_FILE_OPEN_FAILED     __SPX_ERRCODE_FAILED(0x008)
 
-/*** SPXERR_UNEXPECTED_EOF
-*   The end of the file was reached unexpectedly.
-*/
+/// <summary>
+/// The end of the file was reached unexpectedly.
+/// </summary>
 #define SPXERR_UNEXPECTED_EOF       __SPX_ERRCODE_FAILED(0x009)
 
-/*** SPXERR_INVALID_HEADER
-*   One or more arguments are not valid.
-*/
+/// <summary>
+/// Invalid audio header encountered.
+/// </summary>
 #define SPXERR_INVALID_HEADER       __SPX_ERRCODE_FAILED(0x00a)
 
-/*** SPXERR_AUDIO_IS_PUMPING
-*   The requested operation cannot be performed while audio is pumping
-*/
+/// <summary>
+/// The requested operation cannot be performed while audio is pumping
+/// </summary>
 #define SPXERR_AUDIO_IS_PUMPING     __SPX_ERRCODE_FAILED(0x00b)
 
-/*** SPXERR_UNSUPPORTED_FORMAT
-*   The requested operation cannot be performed while audio is pumping
-*/
+/// <summary>
+/// Unsupported audio format.
+/// </summary>
 #define SPXERR_UNSUPPORTED_FORMAT   __SPX_ERRCODE_FAILED(0x00c)
 
-/*** SPXERR_ABORT
-*   Operation aborted.
-*/
+/// <summary>
+/// Operation aborted.
+/// </summary>
 #define SPXERR_ABORT                __SPX_ERRCODE_FAILED(0x00d)
 
-/*** SPXERR_MIC_NOT_AVAILABLE
-*   Microphone is not available.
-*/
+/// <summary>
+/// Microphone is not available.
+/// </summary>
 #define SPXERR_MIC_NOT_AVAILABLE    __SPX_ERRCODE_FAILED(0x00e)
 
-/*** SPXERR_INVALID_STATE
-*   An invalid state was encountered.
-*/
+/// <summary>
+/// An invalid state was encountered.
+/// </summary>
 #define SPXERR_INVALID_STATE        __SPX_ERRCODE_FAILED(0x00f)
 
-/*** SPXERR_UUID_CREATE_FAILED
-*   Attempting to create a UUID failed.
-*/
+/// <summary>
+/// Attempting to create a UUID failed.
+/// </summary>
 #define SPXERR_UUID_CREATE_FAILED   __SPX_ERRCODE_FAILED(0x010)
 
-/*** SPXERR_SETFORMAT_UNEXPECTED_STATE_TRANSITION
-*   An unexpected session state transition was encountered when setting the session audio format. Valid transitions are:
-*   - StartingPump --> ProcessingAudio (at the beginning of stream)
-*   - StoppingPump --> WaitingForAdapterDone (at the end of stream)
-*   - ProcessingAudio --> WaitingForAdapterDone (when the stream runs out of data)
-*   NOTE: All other state transitions are invalid
-*/
+/// <summary>
+/// An unexpected session state transition was encountered when setting the session audio format.
+/// </summary>
+/// <remarks>
+/// Valid transitions are:
+/// * StartingPump --> ProcessingAudio (at the beginning of stream)
+/// * StoppingPump --> WaitingForAdapterDone (at the end of stream)
+/// * ProcessingAudio --> WaitingForAdapterDone (when the stream runs out of data)
+/// All other state transitions are invalid.
+/// </remarks>
 #define SPXERR_SETFORMAT_UNEXPECTED_STATE_TRANSITION __SPX_ERRCODE_FAILED(0x011)
 
-/*** SPXERR_PROCESS_AUDIO_INVALID_STATE
-*   An unexpected session state was encountered in while processing audio. Valid states to encounter are:
-*   - ProcessingAudio: We're allowed to process audio while in this state
-*   - StoppingPump: We're allowed to be called to process audio, but we'll ignore the data passed in while we're attempting to stop the pump
-*   NOTE: All other states are invalid while processing audio.
-*/
+/// <summary>
+/// An unexpected session state was encountered in while processing audio.
+/// </summary>
+/// <remarks>
+/// Valid states to encounter are:
+/// * ProcessingAudio: We're allowed to process audio while in this state.
+/// * StoppingPump: We're allowed to be called to process audio, but we'll ignore the data passed in while we're attempting to stop the pump.
+/// All other states are invalid while processing audio.
+/// </remarks>
 #define SPXERR_PROCESS_AUDIO_INVALID_STATE __SPX_ERRCODE_FAILED(0x012)
 
-/*** SPXERR_START_RECOGNIZING_INVALID_STATE_TRANSITION
-*   An unexpected state transition was encountered while attempting to start recognizing. Valid state transitions are:
-*   - Idle --> StartingPump
-*   NOTE: All other state transitions are invalid when attempting to start recognizing
-*/
+/// <summary>
+/// An unexpected state transition was encountered while attempting to start recognizing.
+/// </summary>
+/// <remarks>
+/// A valid transition is:
+/// * Idle --> StartingPump
+/// All other state transitions are invalid when attempting to start recognizing
+/// </remarks>
 #define SPXERR_START_RECOGNIZING_INVALID_STATE_TRANSITION __SPX_ERRCODE_FAILED(0x013)
 
-/*** SPXERR_UNEXPECTED_CREATE_OBJECT_FAILURE
-*   An unexpected error was encountered when trying to create an internal object.
-*/
+/// <summary>
+/// An unexpected error was encountered when trying to create an internal object.
+/// </summary>
 #define SPXERR_UNEXPECTED_CREATE_OBJECT_FAILURE  __SPX_ERRCODE_FAILED(0x014)
 
-/*** SPXERR_MIC_ERROR
-*   An error in the audio-capturing system.
-*/
+/// <summary>
+/// An error in the audio-capturing system.
+/// </summary>
 #define SPXERR_MIC_ERROR            __SPX_ERRCODE_FAILED(0x015)
 
-/*** SPXERR_NO_AUDIO_INPUT
-*   The requested operation cannot be performed; there is no audio input
-*/
+/// <summary>
+/// The requested operation cannot be performed; there is no audio input.
+/// </summary>
 #define SPXERR_NO_AUDIO_INPUT       __SPX_ERRCODE_FAILED(0x016)
 
-/*** SPXERR_UNEXPECTED_USP_SITE_FAILURE
-*   An unexpected error was encountered when trying to access the USP site
-*/
+/// <summary>
+/// An unexpected error was encountered when trying to access the USP site.
+/// </summary>
 #define SPXERR_UNEXPECTED_USP_SITE_FAILURE  __SPX_ERRCODE_FAILED(0x017)
 
-/*** SPXERR_UNEXPECTED_UNIDEC_SITE_FAILURE
-*   An unexpected error was encountered when trying to access the Unidec site
-*/
+/// <summary>
+/// An unexpected error was encountered when trying to access the Unidec site.
+/// </summary>
 #define SPXERR_UNEXPECTED_UNIDEC_SITE_FAILURE  __SPX_ERRCODE_FAILED(0x018)
