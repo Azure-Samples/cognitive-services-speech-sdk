e1edd9b0352aec3174fb37c617bd08315a36f4bf
Mark Hillebrand <mahilleb@microsoft.com>
2018-05-04 12:21:39 +0000

    Merged PR 620312: try to fix
    try to fix
    
    Related work items: #1117280

--- ci/set-test-variables.sh
+++ ci/set-test-variables.sh
@@ -4,8 +4,9 @@ function vsts_setvar {
   echo "##vso[task.setvariable variable=$1]$2"
 }
 
-vsts_setvar TEST_MODEL_ID 0f7118f0-b39c-4f28-baf0-6deebe25583a
+# Model on internal.cris.ai for Mark; Eldar's subscription for INT/PROD.
+vsts_setvar TEST_MODEL_ID 2c225b03-6216-4b71-9db7-2403c0eb08b3
 
 vsts_setvar TEST_SPEECH_ENDPOINT "wss://westus.stt.speech.microsoft.com/speech/recognition/interactive/cognitiveservices/v1?format=simple&language=en-us"
 
-vsts_setvar TEST_CRIS_ENDPOINT "wss://westus.stt.speech.microsoft.com/speech/recognition/interactive/cognitiveservices/v1?cid=0f7118f0-b39c-4f28-baf0-6deebe25583a"
+vsts_setvar TEST_CRIS_ENDPOINT "wss://westus.stt.speech.microsoft.com/speech/recognition/interactive/cognitiveservices/v1?cid=2c225b03-6216-4b71-9db7-2403c0eb08b3"
