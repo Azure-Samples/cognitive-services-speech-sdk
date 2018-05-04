59499216f5b9f2bb3efdb395df95f310f0bf581c
Mark Hillebrand <mahilleb@microsoft.com>
2018-05-04 14:49:48 +0000

    Merged PR 620370: ci/set-test-variables.sh: switch to new CID
    ci/set-test-variables.sh: switch to new CID
    
    Related work items: #1117280

--- ci/set-test-variables.sh
+++ ci/set-test-variables.sh
@@ -5,8 +5,9 @@ function vsts_setvar {
 }
 
 # Model on internal.cris.ai for Mark; Eldar's subscription for INT/PROD.
-vsts_setvar TEST_MODEL_ID 2c225b03-6216-4b71-9db7-2403c0eb08b3
+TEST_MODEL_ID=6264ab43-c854-4d9f-84fc-5f33621935f3
+vsts_setvar TEST_MODEL_ID $TEST_MODEL_ID
 
 vsts_setvar TEST_SPEECH_ENDPOINT "wss://westus.stt.speech.microsoft.com/speech/recognition/interactive/cognitiveservices/v1?format=simple&language=en-us"
 
-vsts_setvar TEST_CRIS_ENDPOINT "wss://westus.stt.speech.microsoft.com/speech/recognition/interactive/cognitiveservices/v1?cid=2c225b03-6216-4b71-9db7-2403c0eb08b3"
+vsts_setvar TEST_CRIS_ENDPOINT "wss://westus.stt.speech.microsoft.com/speech/recognition/interactive/cognitiveservices/v1?cid=$TEST_MODEL_ID"
