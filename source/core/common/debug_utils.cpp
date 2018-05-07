66692f73a416dcf2346eb13754893ed592e8a753
Mark Hillebrand <mahilleb@microsoft.com>
2018-05-07 08:08:36 +0000

    Merged PR 621724: Revert changes to debug_utils.cpp
    To fix F5 experience in VS.
    
    Related work items: #1117280

--- source/core/common/debug_utils.cpp
+++ source/core/common/debug_utils.cpp
@@ -314,40 +314,26 @@ std::string GetCallStack(size_t skipLevels/* = 0*/, bool makeFunctionNamesStandO
     }
 }
 
-static void ErrorHandler(size_t skipLevels)
+static void SignalHandler()
 {
-    auto callstack = Debug::GetCallStack(skipLevels + 1, false);
+    auto callstack = Debug::GetCallStack(1, false);
 
     SPX_TRACE_VERBOSE(callstack.c_str());
-}
 
-#ifdef _WIN32
-LONG WINAPI WinSignalHandler(EXCEPTION_POINTERS * /*ExceptionInfo*/)
-{
-    ErrorHandler(1);
-    return EXCEPTION_CONTINUE_SEARCH;
-}
-#else
-static void SignalHandler(int sig) {
-    SPX_TRACE_VERBOSE("\nReceived an error signal: %d\n", sig);
-    ErrorHandler(2);
     exit(1);
 }
-#endif
+
+static void SignalHandler(int sig) {
+    SPX_TRACE_VERBOSE("\nReceived an error signal: %d\n", sig);
+    SignalHandler();
+}
 
 void HookSignalHandlers()
 {
-#ifdef _WIN32
-    _set_abort_behavior(0, _WRITE_ABORT_MSG);
-    SetUnhandledExceptionFilter(WinSignalHandler);
-    AddVectoredExceptionHandler(1, WinSignalHandler);
-#else
     signal(SIGSEGV, SignalHandler);
     signal(SIGABRT, SignalHandler);
-    signal(SIGTERM, SignalHandler);
 
-    std::set_terminate([] { ErrorHandler(1); exit(1); });
-#endif
+    std::set_terminate(SignalHandler);
 }
 
 }
