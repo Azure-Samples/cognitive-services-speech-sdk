c53f1ec366d9b7e4be0d55689ee3fe7a2bd57324
Eldar Akchurin <eldak@microsoft.com>
2018-05-16 18:12:57 +0000

    Merged PR 633502: Adding a skeleton project for c# tests
    Adding a skeleton project for c# tests
    
    Related work items: #1232356

---
+++ tests/end2end/csharp/SpeechTests.cs
@@ -0,0 +1,19 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.CognitiveServices.Speech.Tests.EndToEnd
+{
+    [TestClass]
+    public class SmokeTests
+    {
+        [TestMethod]
+        public void NoTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
