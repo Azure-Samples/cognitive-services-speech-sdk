7c555b572e3ddc66348282206fdeff1f7de7e380
Wolfgang Manousek <wolfma61@users.noreply.github.com>
2018-05-24 17:50:39 +0200

    add intent sample for cpp in Windows (#1)

--- Windows/cxx_samples/cxx_samples.cpp
+++ Windows/cxx_samples/cxx_samples.cpp
@@ -13,25 +13,29 @@ extern void SpeechRecognitionWithLanguage();
 extern void SpeechRecognitionWithFile();
 extern void SpeechRecognitionUsingCustomizedModel();
 extern void SpeechContinuousRecognitionUsingEvents();
+
+extern void IntentRecognitionWithMicrophone();
+extern void IntentRecognitionWithLanguage();
+extern void IntentRecognitionWithFile();
+extern void IntentContinuousRecognitionUsingEvents();
+
 extern void TranslationWithMicrophone();
 extern void TranslationWithFile();
 extern void TranslationContinuousRecognitionUsingEvents();
 
-int wmain(int argc, wchar_t **argv)
+void SpeechSamples()
 {
-    cout << "1. Speech recognition with microphone input.\n";
-    cout << "2. Speech recognition in the specified language.\n";
-    cout << "3. Speech recognition with file input.\n";
-    cout << "4. Speech recognition using CRIS model.\n";
-    cout << "5. Speech continuous recognition using events.\n";
-    cout << "6. Translation with microphone input.\n";
-    cout << "7. Translation with file input.\n";
-    cout << "8. Translation continuous recognition using events.\n";
-    cout << "Your choice (0. Exit.): ";
-
     string input;
     do
     {
+        cout << "\nSPEECH RECOGNITION SAMPLES:\n";
+        cout << "1.) Speech recognition with microphone input.\n";
+        cout << "2.) Speech recognition in the specified language.\n";
+        cout << "3.) Speech recognition with file input.\n";
+        cout << "4.) Speech recognition using CRIS model.\n";
+        cout << "5.) Speech continuous recognition using events.\n";
+        cout << "\nChoice (0 for MAIN MENU): ";
+
         input.empty();
         getline(cin, input);
 
@@ -52,19 +56,105 @@ int wmain(int argc, wchar_t **argv)
         case '5':
             SpeechContinuousRecognitionUsingEvents();
             break;
-        case '6':
+        case '0':
+            break;
+        }
+    } while (input[0] != '0');
+}
+
+void IntentSamples()
+{
+    string input;
+    do
+    {
+        cout << "\nINTENT RECOGNITION SAMPLES:\n";
+        cout << "1.) Intent recognition with microphone input.\n";
+        cout << "2.) Intent recognition in the specified language.\n";
+        cout << "3.) Intent recognition with file input.\n";
+        cout << "4.) Intent continuous recognition using events.\n";
+        cout << "\nChoice (0 for MAIN MENU): ";
+
+        input.empty();
+        getline(cin, input);
+
+        switch (input[0])
+        {
+        case '1':
+            IntentRecognitionWithMicrophone();
+            break;
+        case '2':
+            IntentRecognitionWithLanguage();
+            break;
+        case '3':
+            IntentRecognitionWithFile();
+            break;
+        case '4':
+            IntentContinuousRecognitionUsingEvents();
+            break;
+        case '0':
+            break;
+        }
+    } while (input[0] != '0');
+}
+
+void TranslationSamples()
+{
+    string input;
+    do
+    {
+        cout << "\nTRANSLATION SAMPLES:\n";
+        cout << "1.) Translation with microphone input.\n";
+        cout << "2.) Translation with file input.\n";
+        cout << "3.) Translation continuous recognition using events.\n";
+        cout << "\nChoice (0 for MAIN MENU): ";
+
+        input.empty();
+        getline(cin, input);
+
+        switch (input[0])
+        {
+        case '1':
             TranslationWithMicrophone();
             break;
-        case '7':
+        case '2':
             TranslationWithFile();
             break;
-        case '8':
+        case '3':
             TranslationContinuousRecognitionUsingEvents();
             break;
         case '0':
-            cout << "\nStop recognition";
             break;
         }
-        cout << "\nRecognition done. Your Choice (0. Exit): ";
+    } while (input[0] != '0');
+}
+
+int wmain(int argc, wchar_t **argv)
+{
+    string input;
+    do
+    {
+        cout << "\nMAIN MENU\n";
+        cout << "1.) Speech recognition samples.\n";
+        cout << "2.) Intent recognition samples.\n";
+        cout << "3.) Translation samples.\n";
+        cout << "\nChoice (0 to Exit): ";
+
+        input.empty();
+        getline(cin, input);
+
+        switch (input[0])
+        {
+        case '1':
+            SpeechSamples();
+            break;
+        case '2':
+            IntentSamples();
+            break;
+        case '3':
+            TranslationSamples();
+            break;
+        case '0':
+            break;
+        }
     } while (input[0] != '0');
 }
