bd8ee5fac73c8010e45568dc615f3ee053ac6dd9
Mark Hillebrand <mahilleb@microsoft.com>
2018-06-07 14:34:22 +0000

    Merged PR 659505: ci/android/build-android.bat: use vcvars64.bat to pull in nmake
    ci/android/build-android.bat: use vcvars64.bat to pull in nmake
    
    Related work items: #1283386

--- ci/android/build-android.bat
+++ ci/android/build-android.bat
@@ -1,7 +1,22 @@
-set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Tools\MSVC\14.13.26128\bin\HostX86\x86
-rem set SOURCE_ROOT=%~dp0
-set SOURCE_ROOT="%CD%"
+@REM Pull in nmake
+set VSCMD_START_DIR=%CD%
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat" || exit /b 1
+
+set SOURCE_ROOT=%CD%
+
 mkdir build && ^
 cd build && ^
-cmake -G "NMake Makefiles" -Duse_default_uuid=ON -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=26 -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a %SOURCE_ROOT% -DCMAKE_ANDROID_STL_TYPE=c++_static -DOPENSSL_ROOT_DIR=%SOURCE_ROOT%\external\openssl\_install  -DCURL_LIBRARY=%SOURCE_ROOT%\external\curl\_install\lib\libcurl.so -DCURL_INCLUDE_DIR=%SOURCE_ROOT%\external\curl\_install\include -DCMAKE_BUILD_TYPE=%BuildConfiguration% && ^
+cmake -G "NMake Makefiles" ^
+    -Duse_default_uuid=ON ^
+    -DCMAKE_SYSTEM_NAME=Android ^
+    -DCMAKE_SYSTEM_VERSION=26 ^
+    -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang ^
+    -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a ^
+    -DCMAKE_ANDROID_STL_TYPE=c++_static ^
+    -DOPENSSL_ROOT_DIR="%SOURCE_ROOT%\external\openssl\_install" ^
+    -DCURL_LIBRARY="%SOURCE_ROOT%\external\curl\_install\lib\libcurl.so" ^
+    -DCURL_INCLUDE_DIR="%SOURCE_ROOT%\external\curl\_install\include" ^
+    -DCMAKE_BUILD_TYPE="%BuildConfiguration%" ^
+    %SOURCE_ROOT% ^
+  && ^
 cmake --build .
