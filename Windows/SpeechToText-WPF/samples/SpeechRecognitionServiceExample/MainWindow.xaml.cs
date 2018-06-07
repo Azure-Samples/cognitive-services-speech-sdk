d9506ee9e88c5c0331a801b90ff4cf55a9693884
Zhou Wang <zhouwang@microsoft.com>
2018-06-07 11:33:12 +0200

    update copyright; remove unused settings

--- Windows/SpeechToText-WPF/samples/SpeechRecognitionServiceExample/MainWindow.xaml.cs
+++ Windows/SpeechToText-WPF/samples/SpeechRecognitionServiceExample/MainWindow.xaml.cs
@@ -1,27 +1,7 @@
-// <copyright file="MainWindow.xaml.cs" company="Microsoft">
-// Copyright (c) Microsoft Corporation
-// Licensed under the MIT license.
 //
-// MIT License:
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// </copyright>
 
 namespace Microsoft.CognitiveServices.SpeechRecognition
 {
@@ -44,7 +24,7 @@ namespace Microsoft.CognitiveServices.SpeechRecognition
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-      
+
         #region Properties
 
         /// <summary>
