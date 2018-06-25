354a210fc71c563ea3710a92764f67ed49004ea0
Mark Hillebrand <mahilleb@microsoft.com>
2018-06-25 12:57:52 +0000

    Merged PR 680494: Enable build for ARM64
    Enable build for ARM64
    
    Related work items: #1306342

--- ci/android/build-android.bat
+++ ci/android/build-android.bat
@@ -2,16 +2,13 @@
 set VSCMD_START_DIR=%CD%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat" || exit /b 1
 
-set SOURCE_ROOT=%CD%
+set SOURCE_ROOT=%~dp0\..\..
 
-REM ARM32 bit
+REM Determine target architecture (32-bit is default)
 set TARGET_ARCHITECTURE=armeabi-v7a
-if "%BuildPlatformAndroid%" == "arm32" set TARGET_ARCHITECTURE=armeabi-v7a
+if "%BuildPlatform%" == "arm64" set TARGET_ARCHITECTURE=arm64-v8a
 
-REM ARM64 bit (TODO: make this a build parameter)
-if "%BuildPlatformAndroid%" == "arm64" set TARGET_ARCHITECTURE=arm64-v8a
-
-
+pushd "%SOURCE_ROOT%" && ^
 mkdir build && ^
 cd build && ^
 cmake -G "NMake Makefiles" ^
@@ -25,6 +22,6 @@ cmake -G "NMake Makefiles" ^
     -DCURL_LIBRARY="%SOURCE_ROOT%\external\android_ssl_curl\libs\%TARGET_ARCHITECTURE%\lib\libcurl.a" ^
     -DCURL_INCLUDE_DIR="%SOURCE_ROOT%\external\android_ssl_curl\libs\%TARGET_ARCHITECTURE%\include" ^
     -DCMAKE_BUILD_TYPE="%BuildConfiguration%" ^
-    %SOURCE_ROOT% ^
+    .. ^
   && ^
 cmake --build .
