9bb6d7c0da80db4c1714feefee7b55fe3f0a5b2d
Wei Xu <Xu.Wei@microsoft.com>
2018-07-09 20:48:16 +0000

    Merged PR 690377: uwp sample app
    The sample app contains the following:
    1) speech to text from the default microphone
    2) speech to text from files under local or install folder
    3) speech to text from a stream. Users can select any WAV file from any location.
    
    The recognition result is displayed in the lower portion of the window after "Status:"
    The user can click "Enable Microphone" to add microphone support for the app before speech recognition from the microphone.
    
    Related work items: #1317992

---
+++ public_samples/samples/csharp/dotnet/speechtotext-uwp/csharpTestAppUWP/helper.cs
@@ -0,0 +1,68 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+using Windows.Storage.Streams;
+using Windows.Storage;
+using Microsoft.CognitiveServices.Speech;
+using System.Diagnostics;
+using System.IO;
+using System;
+namespace MicrosoftSpeechSDKSamples
+{
+    public class Helper
+    {
+        public static AudioInputStream OpenWaveFile(BinaryReader reader)
+        {
+            AudioInputStreamFormat format = new AudioInputStreamFormat();
+            // Tag "RIFF"
+            char[] data = new char[4];
+            reader.Read(data, 0, 4);
+            if (data[0] != 'R' || data[1] != 'I' || data[2] != 'F' || data[3] != 'F')
+            {
+                throw new global::System.FormatException("Wrong wav header");
+            }
+            
+            // Chunk size
+            long fileSize = reader.ReadInt32();
+            // Subchunk, Wave Header
+            // Subchunk, Format
+            // Tag: "WAVE"
+            reader.Read(data, 0, 4);
+            if ((data[0] != 'W') || (data[1] != 'A') || (data[2] != 'V') || (data[3] != 'E'))
+            {
+                throw new global::System.FormatException("Wrong wav tag in wav header");
+            }
+            // Tag: "fmt"
+            reader.Read(data, 0, 4);
+            if ((data[0] != 'f') || (data[1] != 'm') || (data[2] != 't') && (data[3] != ' '))
+            {
+                throw new global::System.FormatException("Wrong format tag in wav header");
+            }
+            // chunk format size
+            long formatSize = reader.ReadInt32();
+            format.FormatTag = reader.ReadUInt16();
+            format.Channels = reader.ReadUInt16();
+            format.SamplesPerSec = (int)reader.ReadUInt32();
+            format.AvgBytesPerSec = (int)reader.ReadUInt32();
+            format.BlockAlign = reader.ReadUInt16();
+            format.BitsPerSample = reader.ReadUInt16();
+            // Until now we have read 16 bytes in format, the rest is cbSize and is ignored for now.
+            if (formatSize > 16)
+                reader.ReadBytes((int)(formatSize - 16));
+            // Second Chunk, data
+            // tag: data.
+            reader.Read(data, 0, 4);
+
+            if ((data[0] != 'd') || (data[1] != 'a') || (data[2] != 't') || (data[3] != 'a'))
+            {
+                throw new global::System.FormatException("Wrong data tag in wav");
+            }
+            // data chunk size
+            int dataSize = reader.ReadInt32();
+            // now, we have the format in the format parameter and the
+            // reader set to the start of the body, i.e., the raw sample data
+            return new BinaryAudioStreamReader(format, reader);
+        }
+    }
+}
