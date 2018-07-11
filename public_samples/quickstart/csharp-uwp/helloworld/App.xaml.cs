19bfa94fb9f108910fb1ac94d7a5f01811de16f8
Mark Hillebrand <mahilleb@microsoft.com>
2018-07-11 10:14:13 +0000

    Merged PR 697557: Rename C# UWP quick start, add code tags
    Rename C# UWP quick start, add code tags
    
    Related work items: #1313103

--- public_samples/quickstart/csharp-uwp/helloworld/App.xaml.cs
+++ public_samples/quickstart/csharp-uwp/helloworld/App.xaml.cs
@@ -19,7 +19,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace CsharpHelloSpeech
+namespace helloworld
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- public_samples/quickstart/csharp-uwp/helloworld/MainPage.xaml.cs
+++ public_samples/quickstart/csharp-uwp/helloworld/MainPage.xaml.cs
@@ -2,25 +2,14 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
+// </code>
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Microsoft.CognitiveServices.Speech;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
-namespace CsharpHelloSpeech
+namespace helloworld
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -150,3 +139,4 @@ namespace CsharpHelloSpeech
         }
     }
 }
+// </code>
