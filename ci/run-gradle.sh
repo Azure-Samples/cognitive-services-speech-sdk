a711aa6a3dd506e4e757f306803f03fa463e28f2
Mark Hillebrand <mahilleb@microsoft.com>
2018-07-16 17:51:25 +0200

    ci: fix build

---
+++ ci/run-gradle.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+#
+
+USAGE="Usage: $0 [<path-to-maven-repository>]"
+
+if [[ -n $1 ]]; then
+  PACKAGE_PATH="$(cygpath -aw "$1")"
+else
+  PACKAGE_PATH=
+fi
+
+echo $PACKAGE_PATH
+
+set -e -x -o pipefail
+
+readarray -t PROJECTS < <(find . -name gradlew.bat -printf '%h\n')
+
+for dir in "${PROJECTS[@]}"; do
+  echo $dir
+  pushd "$dir"
+  [[ -e gradlew.bat ]]
+  [[ -e build.gradle ]]
+
+  # Patch in local repo if package path specified
+
+  [[ -n $PACKAGE_PATH ]] && {
+
+  cat >> build.gradle <<MAVEN
+allprojects {
+    repositories {
+        maven {
+            url '${PACKAGE_PATH//\\/\\\\}'
+        }
+    }
+}
+MAVEN
+    }
+
+    ./gradlew assemble
+
+  popd
+done
