910bf33c06a718f96e4906ab9ca9234d34018864
Wei Xu <Xu.Wei@microsoft.com>
2018-08-22 15:56:24 +0000

    Merged PR 739845: Load certificates from the system store
    We were hard-coding the certificate in transport.c and feeding it to OpenSSL.
    
    This change is to querying the system store in Windows and loading the certificates from there into OpenSSL.
    
    I did pay attention to the two Windows APIs, CertOpenStore and CertEnumCertificatesInStore are WACK compatible.
    
    **Transport.c:** remove setting option and default certificate string
    
    **tlsio_openssl.c**  see the PR at https://github.com/fmegen/azure-c-shared-utility/pull/3
    
    Related work items: #1377567

--- source/core/usp/transport.c
+++ source/core/usp/transport.c
@@ -810,26 +810,6 @@ void TransportCreateRequestId(TransportHandle transportHandle)
         }
     }
 }
-static char defaultCertString[] = "-----BEGIN CERTIFICATE-----\n"
-"MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ\n"
-"RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD\n"
-"VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTAwMDUxMjE4NDYwMFoX\n"
-"DTI1MDUxMjIzNTkwMFowWjELMAkGA1UEBhMCSUUxEjAQBgNVBAoTCUJhbHRpbW9y\n"
-"ZTETMBEGA1UECxMKQ3liZXJUcnVzdDEiMCAGA1UEAxMZQmFsdGltb3JlIEN5YmVy\n"
-"VHJ1c3QgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKMEuyKr\n"
-"mD1X6CZymrV51Cni4eiVgLGw41uOKymaZN+hXe2wCQVt2yguzmKiYv60iNoS6zjr\n"
-"IZ3AQSsBUnuId9Mcj8e6uYi1agnnc+gRQKfRzMpijS3ljwumUNKoUMMo6vWrJYeK\n"
-"mpYcqWe4PwzV9/lSEy/CG9VwcPCPwBLKBsua4dnKM3p31vjsufFoREJIE9LAwqSu\n"
-"XmD+tqYF/LTdB1kC1FkYmGP1pWPgkAx9XbIGevOF6uvUA65ehD5f/xXtabz5OTZy\n"
-"dc93Uk3zyZAsuT3lySNTPx8kmCFcB5kpvcY67Oduhjprl3RjM71oGDHweI12v/ye\n"
-"jl0qhqdNkNwnGjkCAwEAAaNFMEMwHQYDVR0OBBYEFOWdWTCCR1jMrPoIVDaGezq1\n"
-"BE3wMBIGA1UdEwEB/wQIMAYBAf8CAQMwDgYDVR0PAQH/BAQDAgEGMA0GCSqGSIb3\n"
-"DQEBBQUAA4IBAQCFDF2O5G9RaEIFoN27TyclhAO992T9Ldcw46QQF+vaKSm2eT92\n"
-"9hkTI7gQCvlYpNRhcL0EYWoSihfVCr3FvDB81ukMJY2GQE/szKN+OMY3EU/t3Wgx\n"
-"jkzSswF07r51XgdIGn9w/xZchMB5hbgF/X++ZRGjD8ACtPhSNzkE1akxehi/oCr0\n"
-"Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz\n"
-"ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS\n"
-"R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp\n""-----END CERTIFICATE-----";
 
 TransportHandle TransportRequestCreate(const char* host, void* context, TELEMETRY_HANDLE telemetry, HTTP_HEADERS_HANDLE connectionHeaders, const char* connectionId)
 {
@@ -962,8 +942,6 @@ TransportHandle TransportRequestCreate(const char* host, void* context, TELEMETR
                 // to detect that the network went down.
                 // int val = ANSWER_TIMEOUT_MS;
                 // uws_client_set_option(request->ws.WSHandle, "timeout", &val);
-
-                uws_client_set_option(request->ws.WSHandle, OPTION_TRUSTED_CERT, defaultCertString);
 
             }
         }
