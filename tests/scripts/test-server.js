79cfe37c33dd99e913f188daa811917d634ca3d5
Mark Hillebrand <mahilleb@microsoft.com>
2018-08-31 13:14:12 +0000

    Merged PR 755018: Build: TSA Upload: fix ModernCop log format. Fix logic for TSA Upload (always run in Nightly)
    Build: TSA Upload: fix ModernCop log format. Fix logic for TSA Upload (always run in Nightly)
    
    Related work items: #1392965

--- tests/scripts/test-server.js
+++ tests/scripts/test-server.js
@@ -1,134 +1,136 @@
-'use strict';
+(function () {
+  'use strict';
 
-var WebSocket = require('ws');
-var httpHeaders = require('http-headers');
-var util = require('util');
-var wss = new WebSocket.Server({ port: 8080});
+  var WebSocket = require('ws');
+  var httpHeaders = require('http-headers');
+  var util = require('util');
+  var wss = new WebSocket.Server({ port: 8080});
 
-var prefix = 'Path:%s\r\nContent-Type: application/json; charset=utf-8\r\nX-RequestId:%s\r\n\r\n%j';
-
-
-function rnd(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-var speech = {
-
-  start : function (id, offset) { 
-    return util.format(prefix, 'speech.startDetected', id , { Offset: offset }) ;
-  },
-
-  hypothesis : function (id, offset) { 
-    return util.format(prefix, 'speech.hypothesis', id, {
-      Text: "this is a speech hypothesis",
-      Offset: offset,
-      Duration: offset+23600000
-    });
-  },
-
-  hypothesis_unicode : function (id, offset) { 
-    return util.format(prefix, 'speech.hypothesis', id, {
-      Text: "Найдётся всё. Например, когда появился интернет",
-      Offset: offset,
-      Duration: 26300000
-    });
-  },
-
-  phrase : function (id, offset) { 
-    return util.format(prefix, 'speech.phrase', id, { 
-      RecognitionStatus: "Success", 
-      DisplayText: "Remind me to buy 5 iPhones.", 
-      Offset: offset,
-      Duration: offset+12300000
-    });
-  },
-
-  end : function (id, offset) { 
-    return util.format(prefix, 'speech.endDetected', id, { Offset: offset });
-  },
-};
+  var prefix = 'Path:%s\r\nContent-Type: application/json; charset=utf-8\r\nX-RequestId:%s\r\n\r\n%j';
 
 
-var turn = {
+  function rnd(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 
-  start : function (id, offset) { 
-    return util.format(prefix, 'turn.start', id, { 
-      context: { 
-        serviceTag: '7B33613B91714B32817815DC89633AFA' 
-      } 
-    });
-  },
+  var speech = {
 
-  end : function (id, offset) {
-    return util.format('Path:turn.end\r\nX-RequestId:%s\r\n\r\n', id);
-  },
-};
+    start : function (id, offset) {
+      return util.format(prefix, 'speech.startDetected', id , { Offset: offset }) ;
+    },
 
-var replies = [turn.start, speech.start, speech.hypothesis, speech.hypothesis_unicode, speech.end, speech.phrase, turn.end];
+    hypothesis : function (id, offset) {
+      return util.format(prefix, 'speech.hypothesis', id, {
+        Text: "this is a speech hypothesis",
+        Offset: offset,
+        Duration: offset+23600000
+      });
+    },
 
-var requests = new Set();
+    hypothesis_unicode : function (id, offset) {
+      return util.format(prefix, 'speech.hypothesis', id, {
+        Text: "Найдётся всё. Например, когда появился интернет",
+        Offset: offset,
+        Duration: 26300000
+      });
+    },
 
-var offset = 0;
+    phrase : function (id, offset) {
+      return util.format(prefix, 'speech.phrase', id, {
+        RecognitionStatus: "Success",
+        DisplayText: "Remind me to buy 5 iPhones.",
+        Offset: offset,
+        Duration: offset+12300000
+      });
+    },
 
-wss.on('connection', function connection(ws, req) {
-    console.log('Incoming connection for url '+ req.url + '\n HEADERS:\n' + JSON.stringify(req.headers));
+    end : function (id, offset) {
+      return util.format(prefix, 'speech.endDetected', id, { Offset: offset });
+    },
+  };
 
-    var sub_key = req.headers['ocp-apim-subscription-key'];
-    if (sub_key == 'invalid_key') {
-      ws.close();
-      return;
-    }
 
-    var max_timeout = 100;
+  var turn = {
 
-    try {
-        var piggyback = JSON.parse(sub_key);
-        max_timeout = piggyback['max_timeout']
-    } catch (e) {
-      // oh, well, sub key is not a json, just go with it.
-    }
+    start : function (id, offset) {
+      return util.format(prefix, 'turn.start', id, {
+        context: {
+          serviceTag: '7B33613B91714B32817815DC89633AFA'
+        }
+      });
+    },
 
-    ws.on('message', function incoming(message) {
+    end : function (id, offset) {
+      return util.format('Path:turn.end\r\nX-RequestId:%s\r\n\r\n', id);
+    },
+  };
 
-      if (typeof message === 'string') {  
-       console.log('received message: %s', message);
-      } else {
-        console.log('received binary message');
+  var replies = [turn.start, speech.start, speech.hypothesis, speech.hypothesis_unicode, speech.end, speech.phrase, turn.end];
+
+  var requests = new Set();
+
+  var offset = 0;
+
+  wss.on('connection', function connection(ws, req) {
+      console.log('Incoming connection for url '+ req.url + '\n HEADERS:\n' + JSON.stringify(req.headers));
+
+      var sub_key = req.headers['ocp-apim-subscription-key'];
+      if (sub_key == 'invalid_key') {
+        ws.close();
+        return;
       }
 
-      var headers = httpHeaders(message);
-      var id = headers['x-requestid'].substring(0,32)
+      var max_timeout = 100;
 
-      if (!(requests.has(id))) {
-        console.log('Incoming request with id: %s', id);
-        requests.add(id);
-        offset+=100;
-        (function reply(i, id, connection, offset) {
-          if (i == replies.length) {
-            // https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
-            connection.close(4999, JSON.stringify({error:'Client must close the connection'}));
-            return;
-          }
-          if (replies[i] == speech.hypothesis_unicode && rnd(2) == 0) i--;
-          var response = replies[i++](id, offset++);
-          var timeout = (i != replies.length) ? rnd(max_timeout) : 10000;
-          console.log('replying with: %s\n', response);
-          connection.send(response, {}, function() {
-            setTimeout(function(){reply(i, id, connection, offset)}, timeout) ;
-          });
-        })(0, id, ws, offset);
+      try {
+          var piggyback = JSON.parse(sub_key);
+          max_timeout = piggyback['max_timeout']
+      } catch (e) {
+        // oh, well, sub key is not a json, just go with it.
       }
 
-    });
+      ws.on('message', function incoming(message) {
 
-    ws.on('close', function() {
-      console.log('Connection closed!');
-    });
+        if (typeof message === 'string') {
+         console.log('received message: %s', message);
+        } else {
+          console.log('received binary message');
+        }
 
-    ws.on('error', function(e) {
-      console.log('ERROR:' + e);
-      ws.close(4000, 'Keep clear, we\'re going under...');
-      process.exit(1);
-    });
+        var headers = httpHeaders(message);
+        var id = headers['x-requestid'].substring(0,32)
 
-});
+        if (!(requests.has(id))) {
+          console.log('Incoming request with id: %s', id);
+          requests.add(id);
+          offset+=100;
+          (function reply(i, id, connection, offset) {
+            if (i == replies.length) {
+              // https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
+              connection.close(4999, JSON.stringify({error:'Client must close the connection'}));
+              return;
+            }
+            if (replies[i] == speech.hypothesis_unicode && rnd(2) == 0) i--;
+            var response = replies[i++](id, offset++);
+            var timeout = (i != replies.length) ? rnd(max_timeout) : 10000;
+            console.log('replying with: %s\n', response);
+            connection.send(response, {}, function() {
+              setTimeout(function(){reply(i, id, connection, offset)}, timeout) ;
+            });
+          })(0, id, ws, offset);
+        }
+
+      });
+
+      ws.on('close', function() {
+        console.log('Connection closed!');
+      });
+
+      ws.on('error', function(e) {
+        console.log('ERROR:' + e);
+        ws.close(4000, 'Keep clear, we\'re going under...');
+        process.exit(1);
+      });
+
+  });
+}());
