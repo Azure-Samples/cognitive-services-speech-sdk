23d6770778e3f9d6eaf8335bd0f06ea68f2e1d98
Mark Hillebrand <mahilleb@microsoft.com>
2018-09-12 08:45:30 +0000

    Merged PR 766809: JS: stamp the package with our normal build version
    JS: stamp the package with our normal build version
    
    Related work items: #1405621

--- source/bindings/js/gulpfile.js
+++ source/bindings/js/gulpfile.js
@@ -5,9 +5,6 @@
   var sourcemaps = require('gulp-sourcemaps');
   var tslint = require("gulp-tslint");
   var minify = require('gulp-minify');
-  var git = require('gulp-git');
-  var versionBump = require('gulp-bump')
-  var tagVersion = require('gulp-tag-version');
   var webpack = require('webpack-stream');
 
   gulp.task("build_ES5", function() {
@@ -53,22 +50,4 @@
       .pipe(gulp.dest('distrib'));
   });
 
-
-  // We don't want to release anything without a successful build. So build task is dependency for these tasks.
-  gulp.task('patchRelease', ['build'], function() { return BumpVersionTagAndCommit('patch'); })
-  gulp.task('featureRelease', ['build'], function() { return BumpVersionTagAndCommit('minor'); })
-  gulp.task('majorRelease', ['build'], function() { return BumpVersionTagAndCommit('major'); })
-  gulp.task('preRelease', ['build'], function() { return BumpVersionTagAndCommit('prerelease'); })
-
-  function BumpVersionTagAndCommit(versionType) {
-    return gulp.src(['./package.json'])
-          // bump the version number
-          .pipe(versionBump({type:versionType}))
-          // save it back to filesystem
-          .pipe(gulp.dest('./'))
-          // commit the changed version number
-          .pipe(git.commit('Bumping package version'))
-          // tag it in the repository
-      .pipe(tagVersion());
-  }
 }());
