3cf20433067b515ce78048a6df65c76abb25b5bd
Mark Hillebrand <mahilleb@microsoft.com>
2018-09-19 08:07:04 +0000

    Merged PR 774767: install-build-dependencies.sh: extra diagnostics, attempt to work-around
    install-build-dependencies.sh: extra diagnostics, attempt to work-around package log issue
    
    Related work items: #1414467

--- ci/install-build-dependencies.sh
+++ ci/install-build-dependencies.sh
@@ -10,12 +10,32 @@ SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 case $SPEECHSDK_BUILD_AGENT_PLATFORM in
   Linux-x64)
+
+    # Attempting to diagnose and mitigate the case when we arrive here and
+    # somebody already has taken the lock. ICM 84595724
+    PIDS="$(sudo lsof -t /var/lib/dpkg/lock || true)"
+    if [[ -n $PIDS ]]; then
+      (
+        set +x
+        echo PID for /var/lib/dpkg/lock: $PIDS
+        echo Running processes:
+        sudo ps axjfw
+        echo LSOF
+        sudo lsof /var/lib/dpkg/lock
+        echo Sleeping 30 seconds, then trying to kill.
+        sleep 30
+        sudo lsof -t /var/lib/dpkg/lock | sudo xargs --no-run-if-empty --verbose kill -9
+        echo LSOF after kill
+        sudo lsof /var/lib/dpkg/lock
+        echo DONE
+      )
+    fi
+
     sudo apt-get update
     # https://github.com/Microsoft/vsts-agent-docker/blob/master/ubuntu/16.04/standard/Dockerfile pulls in libssl-dev 1.1 via ppa:ondrej/php.
     # Remove it first, and install the Xenial default one (1.0*)
     sudo apt-get remove --yes libssl-dev
     sudo apt-get install --yes --target-release xenial-updates libssl-dev
-
     sudo apt-get install --yes pkg-config zlib1g-dev libcurl4-openssl-dev libasound2-dev uuid-dev python-dev python-setuptools default-jdk libpcre++-dev bison automake
     sudo "$SCRIPT_DIR/install-swig.sh"
     pip install -U pytest
