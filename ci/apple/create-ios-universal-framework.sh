52ab56d9c8c71a594da9403d9a99fa1c1a38d85c
Mark Hillebrand <mahilleb@microsoft.com>
2018-09-20 14:12:37 +0200

    Pull Request 776734: Build iOS universal frameworks on VSTS
    * use CMake to generate framework builds for device and simulator
    * combine them to a universal framework in a second build step

---
+++ ci/apple/create-ios-universal-framework.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USAGE="Usage: $0 destination "
+
+UNIVERSAL_LIBRARY_DIR="${1?$USAGE}"
+
+set -e
+set -o pipefail
+set -o xtrace
+
+DEVICE_LIBRARY_PATH=build_ios_device/lib
+SIMULATOR_LIBRARY_PATH=build_ios_simulator/lib
+
+# TODO: fix framework name
+FRAMEWORK_NAME=MicrosoftCognitiveServicesSpeech
+
+UNIVERSAL_FRAMEWORK="${UNIVERSAL_LIBRARY_DIR}/${FRAMEWORK_NAME}.framework"
+
+mkdir -p "${UNIVERSAL_LIBRARY_DIR}"
+
+cp -R "${DEVICE_LIBRARY_PATH}/${FRAMEWORK_NAME}.framework" "${UNIVERSAL_LIBRARY_DIR}"
+
+cp -R "${SIMULATOR_LIBRARY_PATH}//${FRAMEWORK_NAME}.framework" "${UNIVERSAL_LIBRARY_DIR}"
+
+lipo \
+    -create -output "${UNIVERSAL_FRAMEWORK}/${FRAMEWORK_NAME}" \
+    "${DEVICE_LIBRARY_PATH}/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" \
+    "${SIMULATOR_LIBRARY_PATH}/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}"
+
+# TODO: copy license
+pushd ${UNIVERSAL_LIBRARY_DIR}
+# zip --symlinks -r "${FRAMEWORK_NAME}-iOS.zip" "license.md" "${FRAMEWORK_NAME}.framework"
+zip --symlinks -r "${FRAMEWORK_NAME}-iOS.zip" "${FRAMEWORK_NAME}.framework"
+popd
+
