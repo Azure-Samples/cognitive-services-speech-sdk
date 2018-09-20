31ab7b27ededf3030272ac44009942ecfe58dd27
Mark Hillebrand <mahilleb@microsoft.com>
2018-09-20 13:27:33 +0200

    Pull Request 776394: [GA, rc2] add TLS 1.2, remove HTTP and LUIS 2nd hop from core
    Add TLS 1.2, remove HTTP and LUIS 2nd hop from core

--- source/core/common/debug_utils.cpp
+++ source/core/common/debug_utils.cpp
@@ -2,8 +2,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
-// http_helpers.cpp: Implementation definitions for *Http* helper methods
-//
 
 #include "stdafx.h"
 #include "debug_utils.h"
