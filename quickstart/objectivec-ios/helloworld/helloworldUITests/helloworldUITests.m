80d03100697d10b8ac9700ced71f1ed79f1d64e8
Christian Landsiedel <chlandsi@microsoft.com>
2018-10-12 12:48:39 +0200

    Update quickstart for iOS

---
+++ quickstart/objectivec-ios/helloworld/helloworldUITests/helloworldUITests.m
@@ -0,0 +1,52 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface helloworldUITests : XCTestCase {
+    XCUIApplication *app;
+}
+@end
+
+@implementation helloworldUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    app = [[XCUIApplication alloc] init];
+    [app launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testRecognizeFromWavFile {
+    // sleep to make sure elements are there
+    [NSThread sleepForTimeInterval:1];
+    XCUIElement * reco_button = app.buttons[@"recognize_file_button"];
+    XCUIElement * result_label = app.otherElements[@"result_label"];
+    
+    XCTAssert(result_label.exists);
+    XCTAssert(reco_button.exists);
+    
+    NSPredicate *pred = [NSPredicate predicateWithFormat:@"label BEGINSWITH 'What'"];
+    
+    [self expectationForPredicate:pred evaluatedWithObject:result_label handler:nil];
+
+    [reco_button tap];
+
+    [self waitForExpectationsWithTimeout:20 handler:nil];
+}
+
+@end
