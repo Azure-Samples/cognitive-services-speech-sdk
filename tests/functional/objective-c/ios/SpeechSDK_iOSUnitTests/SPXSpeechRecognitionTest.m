3167f3bd29ca0ecfb0247cf1b86e797c101d0103
Christian Landsiedel <chlandsi@microsoft.com>
2018-11-29 14:18:53 +0000

    Merged PR 853401: end async tests on SessionStopped event
    fixes a possible race condition with the ordering of events and returning from recognize_once_async.
    
    Related work items: #1526601

--- tests/functional/objective-c/ios/SpeechSDK_iOSUnitTests/SPXSpeechRecognitionTest.m
+++ tests/functional/objective-c/ios/SpeechSDK_iOSUnitTests/SPXSpeechRecognitionTest.m
@@ -46,6 +46,9 @@
         NSLog(@"Received JSON: %@", [eventArgs.result.properties getPropertyById:SPXSpeechServiceResponseJsonResult]);
         finalText = eventArgs.result.text;
         finalResultCount++;
+    }];
+
+    [self.speechRecognizer addSessionStoppedEventHandler: ^ (SPXRecognizer *recognizer, SPXSessionEventArgs *eventArgs) {
         end = true;
     }];
     
@@ -53,9 +56,9 @@
     while (end == false)
         [NSThread sleepForTimeInterval:1.0f];
     [self.speechRecognizer stopContinuousRecognition];
-    
-    XCTAssertEqual(finalResultCount, 1,  "no Final Result Event received");
+
     XCTAssertTrue([finalText isEqualToString:weatherTextEnglish], "Final Result Text does not match");
+    XCTAssertEqual(finalResultCount, 1,  "no Final Result Event received");
 }
 
 - (void)testRecognizeAsync {
@@ -67,15 +70,20 @@
         finalResultCount++;
     }];
     
-    end = false;
-    [self.speechRecognizer recognizeOnceAsync: ^ (SPXSpeechRecognitionResult *result){
-        finalText = result.text;
-        XCTAssertEqual(finalResultCount, 1,  "wrong count of final events");
-        XCTAssertTrue([finalText isEqualToString:weatherTextEnglish], "Final Result Text does not match");
+    [self.speechRecognizer addSessionStoppedEventHandler: ^ (SPXRecognizer *recognizer, SPXSessionEventArgs *eventArgs) {
         end = true;
     }];
+
+    end = false;
+    [self.speechRecognizer recognizeOnceAsync: ^ (SPXSpeechRecognitionResult *result) {
+        finalText = result.text;
+    }];
+
     while (end == false)
         [NSThread sleepForTimeInterval:1.0f];
+
+    XCTAssertTrue([finalText isEqualToString:weatherTextEnglish], "Final Result Text does not match");
+    XCTAssertEqual(finalResultCount, 1,  "wrong count of final events");
 }
 
 - (void)testRecognizeOnce {
@@ -88,32 +96,41 @@
 
     SPXSpeechRecognitionResult *result = [self.speechRecognizer recognizeOnce];
     finalText = result.text;
-    
-    XCTAssertEqual(finalResultCount, 1,  "wrong count of final events");
+
     XCTAssertTrue([finalText isEqualToString:weatherTextEnglish], "Final Result Text does not match");
+    XCTAssertEqual(finalResultCount, 1,  "wrong count of final events");
 }
 
 - (void)testRecognizerWithMultipleHandlers {
     __block int finalResultCount1 = 0;
     __block int finalResultCount2 = 0;
     __block NSString* finalText = @"";
-   
-    [self.speechRecognizer addRecognizedEventHandler: ^ (SPXSpeechRecognizer *recognizer, SPXSpeechRecognitionEventArgs *eventArgs)
-    {
-        finalResultCount1++;
+    __block bool end = false;
+
+    [self.speechRecognizer addRecognizedEventHandler: ^ (SPXSpeechRecognizer *recognizer, SPXSpeechRecognitionEventArgs *eventArgs) {
+            finalResultCount1++;
     }];
 
-    [self.speechRecognizer addRecognizedEventHandler: ^ (SPXSpeechRecognizer *recognizer, SPXSpeechRecognitionEventArgs *eventArgs)
-    {
-        finalResultCount2++;
+    [self.speechRecognizer addRecognizedEventHandler: ^ (SPXSpeechRecognizer *recognizer, SPXSpeechRecognitionEventArgs *eventArgs) {
+            finalResultCount2++;
+    }];
+
+    [self.speechRecognizer addSessionStoppedEventHandler: ^ (SPXRecognizer *recognizer, SPXSessionEventArgs *eventArgs) {
+        end = true;
+    }];
+
+    end = false;
+    [self.speechRecognizer recognizeOnceAsync: ^ (SPXSpeechRecognitionResult *result) {
+        finalText = result.text;
     }];
     
-    SPXSpeechRecognitionResult *result = [self.speechRecognizer recognizeOnce];
-    finalText = result.text;
+    while (end == false)
+        [NSThread sleepForTimeInterval:1.0f];
     
     XCTAssertEqual(finalResultCount1, finalResultCount2,  "Final Result Counts do not match.");
     XCTAssertEqual(finalResultCount1, 1,  "Final Result Count wrong");
     XCTAssertEqual(finalResultCount2, 1,  "Final Result Count wrong");
+    XCTAssertTrue([finalText isEqualToString:weatherTextEnglish], "Final Result Text does not match");
 }
 
 @end
