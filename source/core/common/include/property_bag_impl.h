e1bf01f37487d827d236e31f819a7c8c7640f9e7
Kamil Orujzade <t-kaoruj@microsoft.com>
2019-01-16 13:03:22 +0000

    Merged PR 888292: Hide Sensitive Information in Log
    hide subscription key, authentication token and proxy details in the log
    
    Related work items: #1579626

--- source/core/common/include/property_bag_impl.h
+++ source/core/common/include/property_bag_impl.h
@@ -8,6 +8,7 @@
 #pragma once
 #include <map>
 #include "ispxinterfaces.h"
+#include "property_id_2_name_map.h"
 
 namespace Microsoft {
 namespace CognitiveServices {
@@ -41,7 +42,7 @@ public:
         auto item = m_stringPropertyMap.find(std::string(name));
         if (item != m_stringPropertyMap.end())
         {
-            SPX_DBG_TRACE_VERBOSE_IF(1, "%s: this=0x%8x; name='%s'; value='%s'", __FUNCTION__, this, name, item->second.c_str());
+            LogPropertyAndValue(name, item->second);
             return item->second;
         }
 
@@ -62,7 +63,7 @@ public:
         SPX_IFTRUE_THROW_HR(value == nullptr, SPXERR_INVALID_ARG);
 
         m_stringPropertyMap[std::string(name)] = value;
-        SPX_DBG_TRACE_VERBOSE_IF(1, "%s: this=0x%8x; name='%s'; value='%s'", __FUNCTION__, this, name, value);
+        LogPropertyAndValue(name, value);
     }
 
     bool HasStringValue(const char* name) const override
@@ -95,6 +96,26 @@ protected:
 private:
     mutable std::mutex m_mutexProperties;
     std::map<std::string, std::string> m_stringPropertyMap;
+
+    const std::string m_subKeyName = GetPropertyName(PropertyId::SpeechServiceConnection_Key);
+    const std::string m_authTokenName = GetPropertyName(PropertyId::SpeechServiceAuthorization_Token);
+    const std::string m_proxyPassName = GetPropertyName(PropertyId::SpeechServiceConnection_ProxyPassword);
+    const std::string m_proxyUserName = GetPropertyName(PropertyId::SpeechServiceConnection_ProxyUserName);
+
+    void LogPropertyAndValue(std::string name, std::string value) const
+    {
+        // hide property value for subscription key, authorization token, proxy password and proxy username
+        if (name == m_subKeyName || name == m_authTokenName)
+        {
+            int l = value.length() > 2 ? 2 : 0;
+            value.replace(value.begin(), value.end()-l, value.length()-l, '*');
+        }
+        if ((name == m_proxyPassName || name == m_proxyUserName) && !value.empty())
+        {
+            value = std::string("set to non-empty string");
+        }
+        SPX_DBG_TRACE_VERBOSE("%s: this=0x%8x; name='%s'; value='%s'", __FUNCTION__, this, name.c_str(), value.c_str());
+    }
 };
 
 
