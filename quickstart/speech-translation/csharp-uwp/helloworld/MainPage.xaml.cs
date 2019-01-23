24d11f360d6d424c8df04d6d1d4c6c780f27dc40
erhopf <erhopf@microsoft.com>
2019-01-23 09:27:49 -0800

    Added copyright for UWP translation sample.

--- quickstart/speech-translation/csharp-uwp/helloworld/MainPage.xaml.cs
+++ quickstart/speech-translation/csharp-uwp/helloworld/MainPage.xaml.cs
@@ -1,3 +1,8 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+// <code>
 ﻿using System;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
@@ -170,3 +175,4 @@ namespace helloworld
         }
     }
 }
+// </code>
