68cdf8a1829ff62d29b6aae8ffbf4dbba88e9c5e
Mark Hillebrand <mahilleb@microsoft.com>
2019-03-08 11:23:41 +0000

    Elaborate on some more *recognize*once* comments; fix ipynb

--- samples/python/console/speech_sample.py
+++ samples/python/console/speech_sample.py
@@ -37,10 +37,12 @@ def speech_recognize_once_from_mic():
     # The default language is "en-us".
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 
-    # Perform recognition. `recognize_once` blocks until an utterance has been recognized, after
-    # which recognition stops and a result is returned.  Thus, it is suitable only for single shot
-    # recognition like command or query.  For long-running recognition, use continuous recognitions
-    # instead.
+    # Starts speech recognition, and returns after a single utterance is recognized. The end of a
+    # single utterance is determined by listening for silence at the end or until a maximum of 15
+    # seconds of audio is processed. It returns the recognition text as result.
+    # Note: Since recognize_once() returns only a single utterance, it is suitable only for single
+    # shot recognition like command or query.
+    # For long-running multi-utterance recognition, use start_continuous_recognition() instead.
     result = speech_recognizer.recognize_once()
 
     # Check the result
@@ -65,10 +67,12 @@ def speech_recognize_once_from_file():
     # The default language is "en-us".
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
-    # Perform recognition. `recognize_once` blocks until an utterance has been recognized, after
-    # which recognition stops and a result is returned.  Thus, it is suitable only for single shot
-    # recognition like command or query.  For long-running recognition, use continuous recognitions
-    # instead.
+    # Starts speech recognition, and returns after a single utterance is recognized. The end of a
+    # single utterance is determined by listening for silence at the end or until a maximum of 15
+    # seconds of audio is processed. It returns the recognition text as result.
+    # Note: Since recognize_once() returns only a single utterance, it is suitable only for single
+    # shot recognition like command or query.
+    # For long-running multi-utterance recognition, use start_continuous_recognition() instead.
     result = speech_recognizer.recognize_once()
 
     # Check the result
@@ -99,10 +103,12 @@ def speech_recognize_once_from_file_with_customized_model():
     # The default language is "en-us".
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
-    # Perform recognition. `recognize_once` blocks until an utterance has been recognized, after
-    # which recognition stops and a result is returned.  Thus, it is suitable only for single shot
-    # recognition like command or query.  For long-running recognition, use continuous recognitions
-    # instead.
+    # Starts speech recognition, and returns after a single utterance is recognized. The end of a
+    # single utterance is determined by listening for silence at the end or until a maximum of 15
+    # seconds of audio is processed. It returns the recognition text as result.
+    # Note: Since recognize_once() returns only a single utterance, it is suitable only for single
+    # shot recognition like command or query.
+    # For long-running multi-utterance recognition, use start_continuous_recognition() instead.
     result = speech_recognizer.recognize_once()
 
     # Check the result
@@ -132,10 +138,12 @@ def speech_recognize_once_from_file_with_custom_endpoint_parameters():
     # The default language is "en-us".
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
-    # Perform recognition. `recognize_once` blocks until an utterance has been recognized, after
-    # which recognition stops and a result is returned.  Thus, it is suitable only for single shot
-    # recognition like command or query.  For long-running recognition, use continuous recognitions
-    # instead.
+    # Starts speech recognition, and returns after a single utterance is recognized. The end of a
+    # single utterance is determined by listening for silence at the end or until a maximum of 15
+    # seconds of audio is processed. It returns the recognition text as result.
+    # Note: Since recognize_once() returns only a single utterance, it is suitable only for single
+    # shot recognition like command or query.
+    # For long-running multi-utterance recognition, use start_continuous_recognition() instead.
     result = speech_recognizer.recognize_once()
 
     # Check the result
