569eb209fa1e800229943513215e22ff17244a85
Mark Hillebrand <mahilleb@microsoft.com>
2019-03-13 15:22:42 +0000

    Merged PR 954629: ci/docker/dockertool.pl: add POD
    ci/docker/dockertool.pl: add POD
    
    Where perldoc is available (not in Git Bash), use "perldoc
    dockertool.pl" to read.
    
    Related work items: #1673739

--- ci/docker/dockertool.pl
+++ ci/docker/dockertool.pl
@@ -248,3 +248,176 @@ GetOptions(\%options, @{$commands{$command}{options}}) or die "Stopping.\n";
 $options{extras} = \@ARGV;
 
 $commands{$command}{command}(\%options);
+
+__END__
+
+=head1 NAME
+
+dockertool - helper for managing Speech SDK related Docker images
+
+=head1 SYNOPSIS
+
+B<dockertool>
+B<build>
+[B<--registry> I<reg>]
+[B<--repository> I<repo>]
+[B<--build-arg> I<name=value>]
+[I<image>...]
+
+B<dockertool>
+B<imagetag>
+[B<--registry> I<reg>]
+[B<--repository> I<repo>]
+[I<image>...]
+
+B<dockertool>
+B<push>
+[B<--registry> I<reg>]
+[B<--repository> I<repo>]
+[I<image>...]
+
+B<dockertool>
+B<pull>
+[B<--registry> I<reg>]
+[B<--repository> I<repo>]
+[B<--ignore-error>]
+[B<--try-previous>]
+[I<image>...]
+
+=head1 DESCRIPTION
+
+B<dockertool> is a helper script to manage Docker images related to Speech SDK development.
+These images are defined internally to the script in a data structure named C<%images>.
+For each image three keys are defined, I<version>, I<spec>, and, optionally, I<url>:
+
+=over 4
+
+=item
+
+The version I<version> is a positive integer or the string constant C<'TESTLOCAL'>.
+The version of a certain image is currently used both as definition (which version of the image to build) as well as a reference (which image version is required for the current commit of the Speech SDK, say, for testing samples).
+
+Build tooling external to this script uses the I<TESTLOCAL> version value to indicate that an image should be built on-the-fly rather than pulling it from a registry.
+This provides a cheap way to test image modifications (locally or in Azure DevOps) before pushing out final images to a Docker registry.
+
+=item
+
+The specification I<spec> is an array of references to snippet files stored in the F<snippets/> directory.
+An image's I<Dockerfile> is formed by concatenating the contents of all referenced snippet files.
+
+=item
+
+I<urls> is an array of URL references to download and place next to the (generated) Dockerfile when building images.
+
+=back
+
+The full name (tag) of an image at the Docker level has the form I<REGISTRY/REPO/IMAGE:VERSION>.
+By default, I<REGISTRY> is empty and I<REPO> is I<csspeech>,
+use the B<--registry> and B<--repository> options to override in all commands.
+
+=head1 COMMANDS
+
+The first parameters to B<dockertool> designate a command to run.
+Each command takes a number of options, and (optionally) a list of images to operate on.
+If the list is omitted, the command operates on all images.
+
+=over 4
+
+=item B<build>
+
+Build images locally.
+
+=item B<imagetag>
+
+List the full names of images (including registry, if specified, repository, and version).
+
+=item B<push>
+
+Push images to a registry.
+You must be logged in to the registry already (for example using I<az acr login> for a private Azure Container Registry).
+
+=item B<pull>
+
+Pull images from a registry.
+You must be logged in to the registry already (for example using I<az acr login> for a private Azure Container Registry).
+
+=back
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<--build-arg> I<name=value>
+
+When using the I<build> command, forward this as a B<--build-arg> option to Docker's B<build> command.
+May be specified multiple times.
+
+=item B<--ignore-error>
+
+When using the I<pull> command, return with success even on errors when pulling images.
+
+=item B<--try-previous>
+
+When using the I<pull> command, also attempt to pull the previous version of an image if pulling the current one failed.
+
+=item B<--editor> I<editor>
+
+Use I<editor> to edit the matches.
+If this option is not given the editor defaults to the one specified with environment variable I<EDITOR> or vim.
+
+=item B<--registry> I<reg>
+
+Specify a registry to associate with all images.
+Defaults to empty if not specified.
+
+=item B<--repository> I<repo>
+
+Specify a repository to associate with all images.
+Defaults to I<csspeech> if not specified.
+
+=back
+
+=head1 EXAMPLES
+
+=over 4
+
+=item Log into Speech SDK's private registry and pull all images
+
+  az acr login --name csspeechsdkacr201809
+  perl dockertool.pl pull --registry csspeechsdkacr201809.azurecr.io
+
+=item Log into Speech SDK's private registry and push a new version of an image
+
+  az acr login --name csspeechsdkacr201809
+  perl dockertool.pl push --registry csspeechsdkacr201809.azurecr.io oobedevcpp_ubuntu1804_x64
+
+=item Get full image names in the context of a registry
+
+  perl dockertool.pl imagetag --registry csspeechsdkacr201809.azurecr.io
+
+=item Log into Speech SDK's private registry and pull all images
+
+  az acr login --name csspeechsdkacr201809
+  perl dockertool.pl pull --registry csspeechsdkacr201809.azurecr.io
+
+=item Build an image and run it locally
+
+  fullImage=$(perl dockertool.pl imagetag --repository myuser oobedevcpp_ubuntu1604_x86)
+  perl dockertool.pl build \
+    --repository myuser oobedevcpp_ubuntu1604_x86 \
+    --build-arg BUILD_UID=$(id -u) \
+    --build-user BUILD_USER=$(id -un)
+  docker run --rm -ti $fullImage bash
+
+=back
+
+=head1 CAVEATS
+
+This is simplistic by design.
+The main purpose is to avoid redundancy when defining Dockerfiles for a number of images.
+Alternate approaches using a real text templating engine would work as well.
+
+=head1 COPYRIGHT
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT license.
