1d428087e71411ad7d1c0b39fe1b0d26756b7dd1
PanosPeriorellis <panos.periorellis@microsoft.com>
2019-04-08 15:56:38 +0200

    Update transcription_result.cs

--- samples/batch/csharp/transcription_result.cs
+++ samples/batch/csharp/transcription_result.cs
@@ -2,11 +2,10 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
-
-using System.Collections.Generic;
-
 namespace BatchClient
 {
+    using System.Collections.Generic;
+
     public class AudioFileResult
     {
         public string AudioFileName { get; set; }
