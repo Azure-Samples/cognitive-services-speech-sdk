ab673e6cbb057bdf90c7b27fbc6c2bc2e2d6b5be
PanosPeriorellis <panos.periorellis@microsoft.com>
2019-04-11 14:16:12 +0200

    python sample

---
+++ samples/batch/python/python-client/swagger_client/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.custom_speech_subscriptions_management_api import CustomSpeechSubscriptionsManagementApi
+from swagger_client.api.custom_speech_accuracy_tests_api import CustomSpeechAccuracyTestsApi
+from swagger_client.api.custom_speech_datasets_for_model_adaptation_api import CustomSpeechDatasetsForModelAdaptationApi
+from swagger_client.api.custom_speech_endpoints_api import CustomSpeechEndpointsApi
+from swagger_client.api.custom_speech_models_api import CustomSpeechModelsApi
+from swagger_client.api.custom_speech_transcriptions_api import CustomSpeechTranscriptionsApi
+from swagger_client.api.language_generation_endpoints_api import LanguageGenerationEndpointsApi
+from swagger_client.api.language_generation_models_api import LanguageGenerationModelsApi
+from swagger_client.api.service_health_api import ServiceHealthApi
+from swagger_client.api.voice_datasets_api import VoiceDatasetsApi
+from swagger_client.api.voice_endpoints_api import VoiceEndpointsApi
+from swagger_client.api.voice_models_api import VoiceModelsApi
+from swagger_client.api.voice_tests_api import VoiceTestsApi
