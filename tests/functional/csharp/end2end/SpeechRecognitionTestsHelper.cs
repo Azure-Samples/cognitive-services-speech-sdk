03a8a2b9972b0078a77a9820aa2931a4b53f15f3
Wolfgang Manousek <wolfma@microsoft.com>
2019-04-17 09:56:47 +0000

    Merged PR 1010088: remove multiple consecutive blanks during result normalize
    consecutive blanks confuse our fuzzy result comparison, i.e. in nightly test 'ContinousRecognitionLongRunning'
    changing our normalize implementation on results to remove consecutive blanks
    
    Related work items: #1738840

--- tests/functional/csharp/end2end/SpeechRecognitionTestsHelper.cs
+++ tests/functional/csharp/end2end/SpeechRecognitionTestsHelper.cs
@@ -275,7 +275,8 @@ namespace Microsoft.CognitiveServices.Speech.Tests.EndToEnd
         public static string Normalize(string str)
         {
             str = str.ToLower();
-            return Regex.Replace(str, @"[^\w ]+", "", RegexOptions.Compiled);
+            str =  Regex.Replace(str, @"[^\w ]+", "", RegexOptions.Compiled);
+            return Regex.Replace(str, @"\s+", " ", RegexOptions.Compiled);
         }
 
         public static void AssertConnectionCountMatching(int connectedEventCount, int disconnectedEventCount)
