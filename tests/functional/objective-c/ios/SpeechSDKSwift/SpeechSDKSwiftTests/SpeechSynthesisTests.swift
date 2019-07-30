aa5cdb8378536f282fb26c95a1cb1edc9cd4e527
Yulin Li <yulin.li@microsoft.com>
2019-07-30 08:39:00 +0000

    Merged PR 1148505: Swift test cases for tts part
    Swift test cases for tts part
    
    Related work items: #1903404

---
+++ tests/functional/objective-c/ios/SpeechSDKSwift/SpeechSDKSwiftTests/SpeechSynthesisTests.swift
@@ -0,0 +1,86 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
+import XCTest
+
+class SpeechSynthesisEndToEndTests: XCTestCase {
+    var speechKey: String! = ""
+    var serviceRegion: String! = ""
+
+    var speechConfig: SPXSpeechConfiguration? = nil
+
+    override func setUp() {
+        super.setUp()
+        speechKey = ProcessInfo.processInfo.environment["subscriptionKey"]
+        serviceRegion = ProcessInfo.processInfo.environment["serviceRegion"]
+        self.speechConfig = try! SPXSpeechConfiguration(subscription:self.speechKey, region:self.serviceRegion)
+    }
+    
+    func testSynthesisOutputToResult() {
+        let synthesizer = try! SPXSpeechSynthesizer(speechConfiguration: self.speechConfig!, audioConfiguration: nil)
+        synthesizer.addSynthesisStartedEventHandler() {_, evt in
+            XCTAssertEqual(evt.result.reason, SPXResultReason.synthesizingAudioStarted)
+        }
+        synthesizer.addSynthesizingEventHandler() {_, evt in
+            XCTAssertEqual(evt.result.reason, SPXResultReason.synthesizingAudio)
+            XCTAssertGreaterThan((evt.result.audioData?.count)!, 0)
+        }
+        synthesizer.addSynthesisCompletedEventHandler() {_, evt in
+            XCTAssertEqual(evt.result.reason, SPXResultReason.synthesizingAudioCompleted)
+            XCTAssertGreaterThan((evt.result.audioData?.count)!, 0)
+        }
+        synthesizer.addSynthesisCanceledEventHandler() {_, ent in
+            XCTAssertTrue(false)
+        }
+        let result1 = try! synthesizer.speakText("{{{text1}}}")
+        XCTAssertNotEqual(result1.reason, SPXResultReason.canceled)
+        XCTAssertGreaterThan((result1.audioData?.count)!, 0)
+        let result2 = try! synthesizer.speakText("{{{text1}}}")
+        XCTAssertNotEqual(result2.reason, SPXResultReason.canceled)
+        XCTAssertGreaterThan((result2.audioData?.count)!, 0)
+    }
+    
+    func testSynthesisOutputToAudioDataStream() {
+        let synthesizer = try! SPXSpeechSynthesizer(speechConfiguration: self.speechConfig!, audioConfiguration: nil)
+        let result1 = try! synthesizer.speakText("{{{text1}}}")
+        XCTAssertNotEqual(result1.reason, SPXResultReason.canceled)
+        let stream = try!SPXAudioDataStream(from: result1)
+        let data = NSMutableData(capacity: 1024)
+        var audioLength: Int = 0
+        while (stream.read(data!, length: 1024) > 0) {
+            audioLength += data!.length
+        }
+        XCTAssertGreaterThan(audioLength, 0)
+    }
+    
+    func testSynthesisOutputToPullStream() {
+        let stream: SPXPullAudioOutputStream? = SPXPullAudioOutputStream()
+        let streamConfig: SPXAudioConfiguration? = try!SPXAudioConfiguration(streamOutput: stream!)
+        var synthesizer: SPXSpeechSynthesizer? = try! SPXSpeechSynthesizer(speechConfiguration: self.speechConfig!, audioConfiguration: streamConfig!)
+        try! synthesizer?.startSpeakingText("{{{text1}}}")
+        synthesizer = nil
+        let data = NSMutableData(capacity: 1024)
+        var audioLength: Int = 0
+        while ((stream?.read(data!, length: 1024))! > 0) {
+            audioLength += data!.length
+        }
+        XCTAssertGreaterThan(audioLength, 0)
+    }
+    
+    func testSynthesisOutputToPushStream() {
+        var isClosed = false
+        let stream = SPXPushAudioOutputStream(writeHandler:{data in
+            XCTAssertGreaterThan(data.count, 0)
+            return UInt(data.count)},
+            closeHandler: {
+                isClosed = true
+        })
+        let streamConfig: SPXAudioConfiguration? = try!SPXAudioConfiguration(streamOutput: stream!)
+        var synthesizer: SPXSpeechSynthesizer? = try! SPXSpeechSynthesizer(speechConfiguration: self.speechConfig!, audioConfiguration: streamConfig!)
+        try! synthesizer?.startSpeakingText("{{{text1}}}")
+        synthesizer = nil
+        XCTAssertTrue(isClosed)
+    }
+}
