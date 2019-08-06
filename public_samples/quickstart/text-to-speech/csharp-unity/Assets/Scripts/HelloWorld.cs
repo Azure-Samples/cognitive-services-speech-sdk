6a4fdecd178875ff1b60de6964701c95b10210fc
Yinhe Wei <yinhew@microsoft.com>
2019-08-06 13:48:25 +0000

    Merged PR 1163799: Improve text-to-speech Unity quickstart code to better show the recommended use of synthesizer lifecycle
    Related work items: #1937397

--- public_samples/quickstart/text-to-speech/csharp-unity/Assets/Scripts/HelloWorld.cs
+++ public_samples/quickstart/text-to-speech/csharp-unity/Assets/Scripts/HelloWorld.cs
@@ -19,30 +19,26 @@ public class HelloWorld : MonoBehaviour
     private bool waitingForSpeak;
     private string message;
 
+    private SpeechConfig speechConfig;
+    private SpeechSynthesizer synthesizer;
+
     public void ButtonClick()
     {
-        // Creates an instance of a speech config with specified subscription key and service region.
-        // Replace with your own subscription key and service region (e.g., "westus").
-        var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
-
-        // Creates a speech synthesizer.
-        // Make sure to dispose the synthesizer after use!
-        using (var synthsizer = new SpeechSynthesizer(config, null))
+        lock (threadLocker)
         {
-            lock (threadLocker)
-            {
-                waitingForSpeak = true;
-            }
+            waitingForSpeak = true;
+        }
 
-            // Starts speech synthesis, and returns after a single utterance is synthesized.
-            var result = synthsizer.SpeakTextAsync(inputField.text).Result;
+        string newMessage = string.Empty;
 
+        // Starts speech synthesis, and returns after a single utterance is synthesized.
+        using (var result = synthesizer.SpeakTextAsync(inputField.text).Result)
+        {
             // Checks result.
-            string newMessage = string.Empty;
             if (result.Reason == ResultReason.SynthesizingAudioCompleted)
             {
-                // Since native playback is not yet supported on Unity yet (currently only supported on Windows/Linux Desktop),
-                // use the Unity API to play audio here as a short term solution.
+                // Native playback is not supported on Unity yet (currently only supported on Windows/Linux Desktop).
+                // Use the Unity API to play audio here as a short term solution.
                 // Native playback support will be added in the future release.
                 var sampleCount = result.AudioData.Length / 2;
                 var audioData = new float[sampleCount];
@@ -51,7 +47,7 @@ public class HelloWorld : MonoBehaviour
                     audioData[i] = (short)(result.AudioData[i * 2 + 1] << 8 | result.AudioData[i * 2]) / 32768.0F;
                 }
 
-                // The default output audio format is 16K 16bit mono
+                // The output audio format is 16K 16bit mono
                 var audioClip = AudioClip.Create("SynthesizedAudio", sampleCount, 1, 16000, false);
                 audioClip.SetData(audioData, 0);
                 audioSource.clip = audioClip;
@@ -64,12 +60,12 @@ public class HelloWorld : MonoBehaviour
                 var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
                 newMessage = $"CANCELED:\nReason=[{cancellation.Reason}]\nErrorDetails=[{cancellation.ErrorDetails}]\nDid you update the subscription info?";
             }
+        }
 
-            lock (threadLocker)
-            {
-                message = newMessage;
-                waitingForSpeak = false;
-            }
+        lock (threadLocker)
+        {
+            message = newMessage;
+            waitingForSpeak = false;
         }
     }
 
@@ -95,6 +91,19 @@ public class HelloWorld : MonoBehaviour
             inputField.text = "Enter text you wish spoken here.";
             message = "Click button to synthesize speech";
             speakButton.onClick.AddListener(ButtonClick);
+
+            // Creates an instance of a speech config with specified subscription key and service region.
+            // Replace with your own subscription key and service region (e.g., "westus").
+            speechConfig = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
+
+            // The default format is Riff16Khz16BitMonoPcm.
+            // We are playing the audio in memory as audio clip, which doesn't require riff header.
+            // So we need to set the format to Raw16Khz16BitMonoPcm.
+            speechConfig.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Raw16Khz16BitMonoPcm);
+
+            // Creates a speech synthesizer.
+            // Make sure to dispose the synthesizer after use!
+            synthesizer = new SpeechSynthesizer(speechConfig, null);
         }
     }
 
@@ -112,6 +121,11 @@ public class HelloWorld : MonoBehaviour
                 outputText.text = message;
             }
         }
+    }
+
+    void OnDestroy()
+    {
+        synthesizer.Dispose();
     }
 }
 // </code>
