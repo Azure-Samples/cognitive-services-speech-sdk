8cb07ed1d44a98ac6aa47c29402378e078bd160f
Christian Landsiedel <42998057+chlandsi@users.noreply.github.com>
2019-08-20 17:46:50 +0200

    Add missing import to C# batch client (#357)

--- samples/batch/csharp/batchclient.cs
+++ samples/batch/csharp/batchclient.cs
@@ -11,6 +11,7 @@ namespace BatchClient
     using System.Linq;
     using System.Net;
     using System.Net.Http;
+    using System.Net.Http.Formatting;
     using System.Net.Http.Headers;
     using System.Threading.Tasks;
 
