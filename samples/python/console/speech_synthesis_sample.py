9af31ab9e85ee68a611d9c2a9975f1e35d3fa878
Mark Hillebrand <mahilleb@microsoft.com>
2019-09-27 09:41:37 +0200

    samples/python/console/speech_synthesis_sample.py: add doc string

--- samples/python/console/speech_synthesis_sample.py
+++ samples/python/console/speech_synthesis_sample.py
@@ -3,6 +3,9 @@
 
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+"""
+Speech synthesis samples for the Microsoft Cognitive Services Speech SDK
+"""
 
 try:
     import azure.cognitiveservices.speech as speechsdk
