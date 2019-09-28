69f131343eceb66b24a5f517d9945ea39585d1aa
Ryan Hurey <rhurey@microsoft.com>
2019-09-27 17:58:53 -0700

    Anotehr take on 2 sections

--- quickstart/csharp-dotnet-windows/helloworld/Program.cs
+++ quickstart/csharp-dotnet-windows/helloworld/Program.cs
@@ -3,7 +3,7 @@
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
 
-// <base_code1>
+//<base_code_start>
 using System;
 using System.Threading.Tasks;
 using Microsoft.CognitiveServices.Speech;
@@ -14,29 +14,17 @@ namespace helloworld
     {
         public static async Task RecognizeSpeechAsync()
         {
-// </base_code1>
-// <object_create>
-            // Creates an instance of a speech config with specified subscription key and service region.
-            // Replace with your own subscription key and service region (e.g., "westus").
+// </base_code_start>
+// <object_create_start>
             var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
 
-            // Creates a speech recognizer.
             using (var recognizer = new SpeechRecognizer(config))
             {
-//</object_create>
-                Console.WriteLine("Say something...");
-
-                // Starts speech recognition, and returns after a single utterance is recognized. The end of a
-                // single utterance is determined by listening for silence at the end or until a maximum of 15
-                // seconds of audio is processed.  The task returns the recognition text as result. 
-                // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
-                // shot recognition like command or query. 
-                // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+//</object_create_start>
 //<reco_once>               
                 var result = await recognizer.RecognizeOnceAsync();
 //</reco_once>
 //<results>
-                // Checks result.
                 if (result.Reason == ResultReason.RecognizedSpeech)
                 {
                     Console.WriteLine($"We recognized: {result.Text}");
@@ -58,8 +46,10 @@ namespace helloworld
                     }
                 }
 //</results>
+//<object_create_end>
             }
-// <base_code2>            
+//</object_create_end>
+//<base_code_end>            
         }
 
         static void Main()
@@ -70,4 +60,4 @@ namespace helloworld
         }
     }
 }
-// </base_code2>
+//</base_code_end>
