71a511c6c71d3f3cda6a4f0f3f61696145ccc28c
David Pine <david.pine@microsoft.com>
2020-03-06 15:33:28 -0600

    Removed tabs

--- quickstart/csharp/dotnet/from-blob/program.cs
+++ quickstart/csharp/dotnet/from-blob/program.cs
@@ -1,6 +1,6 @@
-//	
-// Copyright (c) Microsoft. All rights reserved.	
-// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.	
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
 
 using Newtonsoft.Json;
@@ -69,8 +69,7 @@ namespace BatchClient
 
             Uri transcriptionLocation = null;
 
-            var path = $"{SpeechToTextBasePath}Transcriptions/";
-            using (var response = await client.PostAsync(path, sc))
+            using (var response = await client.PostAsync($"{SpeechToTextBasePath}Transcriptions/", sc))
             {
                 if (!response.IsSuccessStatusCode)
                 {
