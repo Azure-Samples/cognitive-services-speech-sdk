515202ce3cf2e32108c1162cb973d976e9de70e1
David Pine <david.pine@microsoft.com>
2020-03-06 15:21:04 -0600

    Added back the copyright

--- quickstart/csharp/dotnet/from-blob/program.cs
+++ quickstart/csharp/dotnet/from-blob/program.cs
@@ -1,3 +1,8 @@
+//	
+// Copyright (c) Microsoft. All rights reserved.	
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.	
+//
+
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -80,7 +85,7 @@ namespace BatchClient
             Console.WriteLine("Checking status.");
 
             var completed = false;
-            
+
             // Check for the status of our transcriptions periodically
             while (!completed)
             {
@@ -194,7 +199,12 @@ namespace BatchClient
 
     public class TranscriptionDefinition
     {
-        TranscriptionDefinition(string name, string description, string locale, Uri recordingsUrl, IEnumerable<ModelIdentity> models)
+        TranscriptionDefinition(
+            string name,
+            string description,
+            string locale,
+            Uri recordingsUrl,
+            IEnumerable<ModelIdentity> models)
         {
             Name = name;
             Description = description;
