520500021fea83b4aa6ec1537ee079c2d402e9c1
John Dempsey <v-demjoh@microsoft.com>
2020-06-24 11:15:33 -0700

    defaults

--- samples/js/node/settings.js
+++ samples/js/node/settings.js
@@ -8,11 +8,11 @@
   
     // Replace with your own subscription key, service region (e.g., "westus"),
     // and recognition language.
-    subscriptionKey:   "f7943ae0fd3046b3949499d6e52a4cdd",
-    serviceRegion:     "westus2", // e.g., "westus"
+    subscriptionKey:   "YourSubscriptionKey",
+    serviceRegion:     "YourServiceRegion", // e.g., "westus"
     language:          "en-US",
   
-    // Replace with the full path to a wav file you want to recognize or overwrite
+    // Replace with the full path to a wav file you want to recognize or overwrite.
     filename:          "C:/t/wavs/normalized/stank.wav", // 16000 Hz, Mono
   
     // Replace with your own Language Understanding subscription key (endpoint
