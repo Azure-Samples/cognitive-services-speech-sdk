f70f035f7021846a72430d0038f7312f9b1abb4e
John Dempsey <v-demjoh@microsoft.com>
2020-06-24 10:59:00 -0700

    used with synthesis, this file is overwritten. please warn user.

--- samples/js/node/settings.js
+++ samples/js/node/settings.js
@@ -8,12 +8,12 @@
   
     // Replace with your own subscription key, service region (e.g., "westus"),
     // and recognition language.
-    subscriptionKey:   "YourSubscriptionKey",
-    serviceRegion:     "YourServiceRegion", // e.g., "westus"
+    subscriptionKey:   "f7943ae0fd3046b3949499d6e52a4cdd",
+    serviceRegion:     "westus2", // e.g., "westus"
     language:          "en-US",
   
-    // Replace with the full path to a wav file you want to recognize.
-    filename:          "YourAudioFile.wav", // 16000 Hz, Mono
+    // Replace with the full path to a wav file you want to recognize or overwrite
+    filename:          "C:/t/wavs/normalized/stank.wav", // 16000 Hz, Mono
   
     // Replace with your own Language Understanding subscription key (endpoint
     // key), region, and app ID in case you want to run the intent sample.
