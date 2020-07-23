3953f12cbb39549447c412b1430e7a3fe5ef1cec
Brian Mouncer <brianem@microsoft.com>
2020-07-23 13:43:25 -0700

    missed some hard tabs.  trying again to remove the hard tabs that breaks the build in the repository validation checks script.

--- samples/batch/csharp/batchclient/failedhttpclientrequestexception.cs
+++ samples/batch/csharp/batchclient/failedhttpclientrequestexception.cs
@@ -5,53 +5,53 @@
 
 namespace BatchClient
 {
-	using System;
-	using System.Net;
-	using System.Runtime.Serialization;
+    using System;
+    using System.Net;
+    using System.Runtime.Serialization;
 
-	[Serializable]
-	public sealed class FailedHttpClientRequestException : Exception
-	{
-		public FailedHttpClientRequestException()
-		{
-			this.StatusCode = HttpStatusCode.Unused;
-		}
+    [Serializable]
+    public sealed class FailedHttpClientRequestException : Exception
+    {
+        public FailedHttpClientRequestException()
+        {
+            this.StatusCode = HttpStatusCode.Unused;
+        }
 
-		public FailedHttpClientRequestException(string message)
-			: base(message)
-		{
-			this.StatusCode = HttpStatusCode.Unused;
-		}
+        public FailedHttpClientRequestException(string message)
+            : base(message)
+        {
+            this.StatusCode = HttpStatusCode.Unused;
+        }
 
-		public FailedHttpClientRequestException(string message, Exception exception)
-			: base(message, exception)
-		{
-			this.StatusCode = HttpStatusCode.Unused;
-		}
+        public FailedHttpClientRequestException(string message, Exception exception)
+            : base(message, exception)
+        {
+            this.StatusCode = HttpStatusCode.Unused;
+        }
 
-		public FailedHttpClientRequestException(HttpStatusCode status, string reasonPhrase)
-			: base(reasonPhrase)
-		{
-			this.StatusCode = status;
-		}
+        public FailedHttpClientRequestException(HttpStatusCode status, string reasonPhrase)
+            : base(reasonPhrase)
+        {
+            this.StatusCode = status;
+        }
 
-		private FailedHttpClientRequestException(SerializationInfo info, StreamingContext context)
-			: base(info, context)
-		{
-			this.StatusCode = (HttpStatusCode)info.GetValue(nameof(this.StatusCode), typeof(HttpStatusCode));
-		}
+        private FailedHttpClientRequestException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            this.StatusCode = (HttpStatusCode)info.GetValue(nameof(this.StatusCode), typeof(HttpStatusCode));
+        }
 
-		public HttpStatusCode StatusCode { get; private set; }
+        public HttpStatusCode StatusCode { get; private set; }
 
-		public string ReasonPhrase => this.Message;
+        public string ReasonPhrase => this.Message;
 
-		public override void GetObjectData(SerializationInfo info, StreamingContext context)
-		{
-			if (info != null)
-			{
-				info.AddValue(nameof(this.StatusCode), this.StatusCode);
-				base.GetObjectData(info, context);
-			}
-		}
-	}
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info != null)
+            {
+                info.AddValue(nameof(this.StatusCode), this.StatusCode);
+                base.GetObjectData(info, context);
+            }
+        }
+    }
 }
