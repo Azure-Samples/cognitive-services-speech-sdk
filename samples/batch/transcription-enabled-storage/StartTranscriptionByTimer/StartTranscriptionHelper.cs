a6fff18eb313786b77fe8c5d5e61a4261e5b2a17
Jan Henry van der Vegte <a-javan@microsoft.com>
2020-09-22 16:47:16 +0200

    fix pipeline errors

--- samples/batch/transcription-enabled-storage/StartTranscriptionByTimer/StartTranscriptionHelper.cs
+++ samples/batch/transcription-enabled-storage/StartTranscriptionByTimer/StartTranscriptionHelper.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="StartTranscriptionHelper.cs" company="Microsoft Corporation">
+// <copyright file="StartTranscriptionHelper.cs" company="Microsoft Corporation">
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 // </copyright>
