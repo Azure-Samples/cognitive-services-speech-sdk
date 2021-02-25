fb162d4fd015d91af5fd4ef99986c20a5652468d
hacker1 <hacker1lma@OTAPRD812ops.onmicrosoft.com>
2021-02-25 16:31:25 +0530

    updated based on review - 1. indent 2. added single space after // 3. removed all commented code (except for features that are optional in the sample) 4. removed unused code 5. removed to be worked on code 6. created empty opts - removed numbers

--- samples/batch/js/node/main.js
+++ samples/batch/js/node/main.js
@@ -1,71 +1,32 @@
-
-
 const { TranscriptionProperties } = require('speech_to_text_api_v30');
 var SpeechToTextApiV30 = require('speech_to_text_api_v30');
 var sleep = require('system-sleep');
 var defaultClient = SpeechToTextApiV30.ApiClient.instance;
 const request = require('request');
 
-   //Create API Instance
-   var apiInstance = new SpeechToTextApiV30.DefaultApi();
+// Create API Instance
+var apiInstance = new SpeechToTextApiV30.DefaultApi();
 // Your subscription key and region for the speech service
-const SUBSCRIPTION_KEY = ""
 
-const SERVICE_REGION = "centralindia"
-
+const API_KEY=""
 const NAME = "Simple transcription"
 const DESCRIPTION = "Simple transcription description"
-
 const LOCALE = "en-US"
+
+// Provide the uri of the audio file 
 const RECORDINGS_BLOB_URI = ""
 
 // Provide the uri of a container with audio files for transcribing all of them with a single request
 const RECORDINGS_CONTAINER_URI = ""
 
-//Set model information when doing transcription with custom models
-const MODEL_REFERENCE = 'None' ; // guid of a custom model
-
-var callback2 = function(error, data, response) {
-        var completed = "False";
-        if (error) {
-          console.error(error);
-        } else {
-          console.log('API called successfully. Returned data ' );
-          var jsonParsed = JSON.parse(JSON.stringify(data));
-          console.log(jsonParsed.status);
-          var trans_status = jsonParsed.status;
-
-          if (trans_status == "Failed" || trans_status == "Succeeded")
-          {
-            completed = "True"
-          }
-        if (trans_status == "Succeeded")
-          {
-           
-          }
-        else if (trans_status == "Failed")
-            console.log("Transcription failed: {transcription.properties.error.message}")
-
-        }
-      };
-
- var callback = function(error, data, response) {
-  if (error) {
-    console.error(error);
-  } else {
-   console.log('API called successfully. Returned data: ' + JSON.stringify(data));
-
-  }
-};
-
 var callbackGetTrans = function(error, data, response) {
   if (error) {
     console.error(error);
   } else {
-   console.log('Callback Get API called successfully. Returned data: ' + JSON.stringify(data));
+    console.log('Callback Get Transcription API called successfully. Returned data: ' + JSON.stringify(data));
     completed=false ;
     var tid = data.self.substring(data.self.lastIndexOf("/")+1);
-    console.log("Trans Id " + tid);
+    console.log("Transcription Id " + tid);
     status = _checkComplete(tid);
 
     if (status == "Succeeded")
@@ -84,56 +45,46 @@ var callbackGetTransFiles = function(error, data, response) {
   } else {
    console.log('callbackGetTransFiles API called successfully. Returned data: ' + JSON.stringify(data));
    pag_files = data.values;
-   console.log(pag_files.length);
-   for (var i = 0; i < pag_files.length; i++){
+   for (var i = 0; i < pag_files.length; i++)
+   {
      var file_data = pag_files[i];
-     var url = file_data.links.contentUrl;
-    
-    console.log (" url " + url);
-     if(file_data.kind!='Transcription')
+     var url = file_data.links.contentUrl; 
+     if(file_data.kind !='Transcription')
         continue;
      else{
         request(url, { json: true }, (err, res, body) => {
             if (err) { 
                 return console.log(err); 
             }
-         //   console.log("***************************************************************  response "+JSON.stringify(body));
-            console.log("***************************************************************************");
-
-            console.log(body);
-            //content.decode('utf-8')
+            else {
+              console.log("\n\n\n Name of file : " + file_data.name);
+              console.log("\n Transcription File Content is : " + JSON.stringify(body));
+            }
         });
         
      }
    }
   }
 };
-        
-//_transcribe();
-//_deleteAllTranscriptions();
+
+// uncomment to execute 
+// _transcribe();
+// _deleteAllTranscriptions();
 
 function _transcribe()
 {
     
     // Configure API key authorization: apiKeyHeader
     var apiKeyHeader = defaultClient.authentications['apiKeyHeader'];
-    apiKeyHeader.apiKey = '';
+    apiKeyHeader.apiKey = API_KEY;
     // Configure API key authorization: apiKeyQuery
     var apiKeyQuery = defaultClient.authentications['apiKeyQuery'];
-    apiKeyQuery.apiKey = '';
+    apiKeyQuery.apiKey = API_KEY;
 
-    //Create API Instance
-    //var apiInstance = new SpeechToTextApiV30.DefaultApi();
-
-    const subscriptionKey = "{your-subscription-key-here}";
-    const serviceRegion = "centralindia"; // e.g., "westus"
-    defaultClient.basePath = 'https://centralindia.api.cognitive.microsoft.com/speechtotext/v3.0'.replace(/\/+$/, '');
+    defaultClient.basePath = 'https:// centralindia.api.cognitive.microsoft.com/speechtotext/v3.0'.replace(/\/+$/, '');
 
     // Uncomment this block to transcribe one file.
-    //var opts = _transcribeFromSingleBlob(RECORDINGS_BLOB_URI);
-
-    // Uncomment this block to use custom models for transcription.
-    // transcription_definition = transcribe_with_custom_model(api, RECORDINGS_BLOB_URI, properties)
+    // var opts = _transcribeFromSingleBlob(RECORDINGS_BLOB_URI);
 
     // Uncomment this block to transcribe all files from a container.
     var opts = _transcribeFromContainer(RECORDINGS_CONTAINER_URI);
@@ -145,72 +96,61 @@ function _transcribe()
         else {
           var completed = false;
           var tid = data.self.substring(data.self.lastIndexOf("/")+1);
-          console.log("Transaction ID is :"+tid);
+          console.log("Transcription ID created is : "+tid);
           apiInstance.getTranscription(tid,callbackGetTrans);
           
         }
       });
     
-}//end of _transcribe()
+}// end of _transcribe()
 
 
 function _deleteAllTranscriptions(){
     
-    //Delete all transcriptions associated with your speech resource.
-    console.log("Deleting all existing completed transcriptions.")
-    // Configure API key authorization: apiKeyHeader
-    var apiKeyHeader = defaultClient.authentications['apiKeyHeader'];
-    apiKeyHeader.apiKey = '';
-    // Configure API key authorization: apiKeyQuery
-    var apiKeyQuery = defaultClient.authentications['apiKeyQuery'];
-    apiKeyQuery.apiKey = '';
+  // Delete all transcriptions associated with your speech resource.
+  console.log("Deleting all existing completed transcriptions.")
+  // Configure API key authorization: apiKeyHeader
+  var apiKeyHeader = defaultClient.authentications['apiKeyHeader'];
+  apiKeyHeader.apiKey = API_KEY;
+  // Configure API key authorization: apiKeyQuery
+  var apiKeyQuery = defaultClient.authentications['apiKeyQuery'];
+  apiKeyQuery.apiKey = API_KEY;
+  
+  defaultClient.basePath = 'https:// centralindia.api.cognitive.microsoft.com/speechtotext/v3.0'.replace(/\/+$/, '');
 
-    //Create API Instance
-    //var apiInstance = new SpeechToTextApiV30.DefaultApi();
-
-    const subscriptionKey = "{your-subscription-key-here}";
-    const serviceRegion = "centralindia"; // e.g., "westus"
-    defaultClient.basePath = 'https://centralindia.api.cognitive.microsoft.com/speechtotext/v3.0'.replace(/\/+$/, '');
-
-    // get all transcriptions for the subscription
-     var opts = { 
-        'skip': 56, // Number | Format - int32. Number of transcriptions that will be skipped.
-        'top': 56 // Number | Format - int32. Number of transcriptions that will be included after skipping.
-      };
-      
-      
-      var callback = function(error, data, response) {
+  // get all transcriptions for the subscription
+  var opts = { 
+  };
+  var callback = function(error, data, response) {
         if (error) {
           console.error(error);
         } else {
-          //console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+          // uncomment to display the response of Delete All transcriptions
+          // console.log('Delete all transcriptions API called successfully. Returned data: ' + JSON.stringify(data));
           pag_files = data.values;
           console.log(pag_files.length);
           for (var i = 0; i < pag_files.length; i++){
             var file_data = pag_files[i];
             var tid = file_data.self.substring(file_data.self.lastIndexOf("/")+1);
-           console.log (" tid " + tid);
             if(file_data.status=='Succeeded' || file_data.status == 'Failed'){
-               console.log("status failed or success");
+               console.log("Deleting Transcription with status failed or success " + tid);
                var callback = function(error, data, response) {
                 if (error) {
                   console.error(error);
                 } else {
-                  console.log('API called successfully.');
+                  console.log('Transcription deleted - API called successfully.'+tid);
                 }
               };
               apiInstance.deleteTranscription(tid, callback);
-                
-
             }
             else{
-                console.log("status running" + file_data.status);
+                console.log("Status running" + file_data.status);
             }
         }
       };
-    }//end else
+    }// end else
       apiInstance.getTranscriptions(opts, callback);
-}//end deleteAllTranscriptions
+}// end deleteAllTranscriptions
 
 
 
@@ -242,50 +182,14 @@ function _transcribeFromSingleBlob(uri){
           "description" : DESCRIPTION
         };
         return opts;
-}//end of _transcribeFromSingleBlob
-
-
-//To be worked on 
-function _transcribeWithCustomModel(api, uri){
-
-    //Transcribe a single audio file located at `uri` using the settings specified in `properties`
-    //using the base model for the specified locale.
-    
-    // Model information (ADAPTED_ACOUSTIC_ID and ADAPTED_LANGUAGE_ID) must be set above.
-    if (MODEL_REFERENCE == 'None')
-    {
-        console.log("Custom model ids must be set when using custom models");
-    }
-    //model = apiInstance.getModel(MODEL_REFERENCE);
-    var opts = { 
-        // Transcription | The details of the new transcription.
-            'transcription' : new SpeechToTextApiV30.Transcription()
-        }
-    
-        opts.transcription = 
-        {
-            "contentUrls": [
-                uri
-            ],
-          "properties": {
-          "wordLevelTimestampsEnabled": true
-          },
-          "locale": LOCALE,
-          "displayName": NAME + new Date(),
-          "description" : DESCRIPTION,
-           "model" :  model
-        };
-        return opts;
-
-    
-}//_transcribeWithCustomModel
+}// end of _transcribeFromSingleBlob
 
 function _transcribeFromContainer(uri){
+    console.log("_transcribeFromContainer");
     var opts = { 
         // Transcription | The details of the new transcription.
             'transcription' : new SpeechToTextApiV30.Transcription()
         }
-    
         opts.transcription = 
         {
           "contentContainerUrl": uri,
@@ -297,7 +201,7 @@ function _transcribeFromContainer(uri){
           "description" : DESCRIPTION
         };
         return opts;
-}//end of _transcribeFromSingleBlob
+}// end of _transcribeFromSingleBlob
 
 
 function _checkComplete(tid)
@@ -307,7 +211,6 @@ function _checkComplete(tid)
   while(completed!=true)
   {
   // wait for 5 seconds before refreshing the transcription status
-
     sleep(5000);
     
     apiInstance.getTranscription(tid, function(error, data, response) 
@@ -327,7 +230,7 @@ function _checkComplete(tid)
     }
     );
   
-  }//end while
+  }// end while
   return status; 
   
 }
