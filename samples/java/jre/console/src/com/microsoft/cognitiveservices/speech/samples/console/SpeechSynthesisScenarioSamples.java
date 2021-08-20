edcfee4084a1b368ce65f8aa21a26a3f5da4982d
Yulin Li <liyulin@pku.edu.cn>
2021-08-20 15:10:47 +0800

    copyright

--- samples/java/jre/console/src/com/microsoft/cognitiveservices/speech/samples/console/SpeechSynthesisScenarioSamples.java
+++ samples/java/jre/console/src/com/microsoft/cognitiveservices/speech/samples/console/SpeechSynthesisScenarioSamples.java
@@ -1,3 +1,7 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
 package com.microsoft.cognitiveservices.speech.samples.console;
 
 import com.microsoft.cognitiveservices.speech.*;
