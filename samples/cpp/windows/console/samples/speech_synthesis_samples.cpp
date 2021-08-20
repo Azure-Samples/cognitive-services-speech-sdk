539e3428c9fa12470def2e9c25fbac88ccd5ed42
John Dempsey <v-demjoh@microsoft.com>
2021-08-20 16:14:02 -0700

    ChristopherNeural experiment (#1222)
    * MelindaMa experiment
    
    * Update .project
    
    * Update .project
    
    * Update org.eclipse.buildship.core.prefs
    
    * don't introduce these
    
    Co-authored-by: Ryan Hurey <RHUREY@MICROSOFT.COM>

--- samples/cpp/windows/console/samples/speech_synthesis_samples.cpp
+++ samples/cpp/windows/console/samples/speech_synthesis_samples.cpp
@@ -114,11 +114,11 @@ void SpeechSynthesisWithVoice()
     auto config = SpeechConfig::FromSubscription("YourSubscriptionKey", "YourServiceRegion");
 
     // Sets the voice name.
-    // e.g. "Microsoft Server Speech Text to Speech Voice (en-US, JennyNeural)".
+    // e.g. "Microsoft Server Speech Text to Speech Voice (en-US, ChristopherNeural)".
     // The full list of supported voices can be found here:
     // https://aka.ms/csspeech/voicenames
     // And, you can try GetVoicesAsync method to get all available voices (see SpeechSynthesisGetAvailableVoices() sample below).
-    auto voice = "Microsoft Server Speech Text to Speech Voice (en-US, GuyNeural)";
+    auto voice = "Microsoft Server Speech Text to Speech Voice (en-US, ChristopherNeural)";
     config->SetSpeechSynthesisVoiceName(voice);
 
     // Creates a speech synthesizer for the specified voice, using the default speaker as audio output.
