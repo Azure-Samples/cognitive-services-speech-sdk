5792d552dc2f5ba142d305e154ad97abe5f2b069
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2022-07-14 10:44:21 -0700

    Update Chunk.cs

--- scenarios/csharp/dotnetcore/call_center/call_center/Chunk.cs
+++ scenarios/csharp/dotnetcore/call_center/call_center/Chunk.cs
@@ -1,8 +1,10 @@
-// Source:
-// https://github.com/dotnet/runtime/blob/main/src/libraries/System.Linq/src/System/Linq/Chunk.cs
-
+//
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+//
+
+// Source:
+// https://github.com/dotnet/runtime/blob/main/src/libraries/System.Linq/src/System/Linq/Chunk.cs
 
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
