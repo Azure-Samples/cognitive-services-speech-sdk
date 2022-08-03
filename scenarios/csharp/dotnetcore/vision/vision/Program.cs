ba3b2224f766a221b727150a37001d7a9d1ac950
v-jaswel <v-jasonwells@microsoft.com>
2022-08-03 08:49:51 -0700

    Update Vision scenario

--- scenarios/csharp/dotnetcore/vision/vision/Program.cs
+++ scenarios/csharp/dotnetcore/vision/vision/Program.cs
@@ -1,1 +1,82 @@
-.
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vision
+{
+    public class Program
+    {
+        private UserConfig m_userConfig;
+        
+        // How long to wait while polling TODO1.
+        private const int waitSeconds = 10;
+
+        private const string imageAnalysisPath = "/vision/v4.0-preview.1/operations/imageanalysis:analyze";
+
+        private async Task<string> AnalyzeImage()
+        {
+            var uri = new UriBuilder(Uri.UriSchemeHttps, m_userConfig.endpoint);
+            uri.Path = imageAnalysisPath;
+            var content = new
+            {
+                url = m_userConfig.imageAnalysisURL
+            };
+            var response = await RestHelper.SendPost(uri.Uri.ToString(), JsonSerializer.Serialize(content), m_userConfig.subscriptionKey, new HttpStatusCode[]{ HttpStatusCode.OK });
+            return response.content;
+        }
+
+        // Note: To pass command-line arguments, run:
+        // dotnet run -- [args]
+        // For example:
+        // dotnet run -- --help
+        public async Task Run()
+        {
+            var result = await AnalyzeImage();
+            Console.WriteLine(result);
+            return;
+        }
+
+        public async static Task Main(string[] args)
+        {
+            string usage = @"USAGE: dotnet run -- [...]
+
+  HELP
+    --help                          Show this help and stop.
+
+  CONNECTION
+    --key KEY                       Your Azure Computer Vision subscription key. Required.
+    --endpoint ENDPOINT             Your Azure Computer Vision endpoint. Required.
+
+  INPUT
+    --input URL                     The URL of the image you want to analyze. Required.
+";
+            
+            try
+            {
+                if (args.Contains("--help"))
+                {
+                    Console.WriteLine(usage);
+                }
+                else
+                {
+                    await (new Program { m_userConfig = new UserConfig(args, usage) }).Run();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }            
+        }
+    }
+}
--- scenarios/csharp/dotnetcore/vision/vision/UserConfig.cs
+++ scenarios/csharp/dotnetcore/vision/vision/UserConfig.cs
@@ -1,1 +1,63 @@
-.
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
+using System;
+using System.Linq;
+
+namespace Vision
+{
+    public class UserConfig
+    {
+        /// The URL of the image to analyze.
+        readonly public string imageAnalysisURL;
+        /// The subscription key for your Computer Vision subscription.
+        readonly public string subscriptionKey;
+        /// The endpoint for your Computer Vision subscription.
+        readonly public string endpoint;
+
+        public static string? GetCmdOption(string[] args, string option)
+        {
+            int index = Array.FindIndex(args, x => x.Equals(option, StringComparison.OrdinalIgnoreCase));
+            if (index > -1 && index < args.Length - 1)
+            {
+                // We found the option (for example, "--output"), so advance from that to the value (for example, "filename").
+                return args[index + 1];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static bool CmdOptionExists(string[] args, string option)
+        {
+            return args.Contains(option);
+        }
+
+        public UserConfig(string[] args, string usage)
+        {
+            string? subscriptionKey = GetCmdOption(args, "--key");
+            if (subscriptionKey is null)
+            {
+                throw new ArgumentException($"Missing Computer Vision subscription key.{Environment.NewLine}Usage: {usage}");
+            }
+            string? endpoint = GetCmdOption(args, "--endpoint");
+            if (endpoint is null)
+            {
+                throw new ArgumentException($"Missing Computer Vision endpoint.{Environment.NewLine}Usage: {usage}");
+            }
+
+            string? imageAnalysisURL = GetCmdOption(args, "--input");
+            if (imageAnalysisURL is null)
+            {
+                throw new ArgumentException($"Missing URL of image to analyze.{Environment.NewLine}Usage: {usage}");
+            }
+            
+            this.imageAnalysisURL = imageAnalysisURL;
+            this.subscriptionKey = subscriptionKey;
+            this.endpoint = endpoint;
+        }
+    }
+}
