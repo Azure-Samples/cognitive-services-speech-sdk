a33719b7b6af885bc0ad677aa83591b1ea8cda0d
KilloranFiander <109238433+KilloranFiander@users.noreply.github.com>
2022-08-17 09:50:41 -0700

    JavaCodeUpdatesMatchedFromDocSite

--- quickstart/java/jre/from-microphone/src/speechsdk/quickstart/Main.java
+++ quickstart/java/jre/from-microphone/src/speechsdk/quickstart/Main.java
@@ -3,68 +3,48 @@
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
 // <code>
-package speechsdk.quickstart;
-
-import java.util.concurrent.Future;
 import com.microsoft.cognitiveservices.speech.*;
+import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
 
-/**
- * Quickstart: recognize speech using the Speech SDK for Java.
- */
-public class Main {
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
-    /**
-     * @param args Arguments are ignored in this sample.
-     */
-    public static void main(String[] args) {
+public class SpeechRecognition {
+    private static String YourSubscriptionKey = "YourSubscriptionKey";
+    private static String YourServiceRegion = "YourServiceRegion";
 
-        // Replace below with your own subscription key
-        String speechSubscriptionKey = "YourSubscriptionKey";
-        // Replace below with your own service region (e.g., "westus").
-        String serviceRegion = "YourServiceRegion";
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        SpeechConfig speechConfig = SpeechConfig.fromSubscription(YourSubscriptionKey, YourServiceRegion);
+        speechConfig.setSpeechRecognitionLanguage("en-US");
+        recognizeFromMicrophone(speechConfig);
+    }
 
-        // Creates an instance of a speech recognizer using speech configuration with specified
-        // subscription key and service region and microphone as default audio input.
-        try (SpeechConfig config = SpeechConfig.fromSubscription(speechSubscriptionKey, serviceRegion);
-             SpeechRecognizer reco = new SpeechRecognizer(config)) {
+    public static void recognizeFromMicrophone(SpeechConfig speechConfig) throws InterruptedException, ExecutionException {
+        AudioConfig audioConfig = AudioConfig.fromDefaultMicrophoneInput();
+        SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
 
-            assert(config != null);
-            assert(reco != null);
-            int exitCode = 1;
+        System.out.println("Speak into your microphone.");
+        Future<SpeechRecognitionResult> task = speechRecognizer.recognizeOnceAsync();
+        SpeechRecognitionResult speechRecognitionResult = task.get();
 
-            System.out.println("Say something...");
-
-            Future<SpeechRecognitionResult> task = reco.recognizeOnceAsync();
-            assert(task != null);
-
-            SpeechRecognitionResult result = task.get();
-            assert(result != null);
-
-            if (result.getReason() == ResultReason.RecognizedSpeech) {
-                System.out.println("We recognized: " + result.getText());
-                exitCode = 0;
-            }
-            else if (result.getReason() == ResultReason.NoMatch) {
-                System.out.println("NOMATCH: Speech could not be recognized.");
-            }
-            else if (result.getReason() == ResultReason.Canceled) {
-                CancellationDetails cancellation = CancellationDetails.fromResult(result);
-                System.out.println("CANCELED: Reason=" + cancellation.getReason());
-
-                if (cancellation.getReason() == CancellationReason.Error) {
-                    System.out.println("CANCELED: ErrorCode=" + cancellation.getErrorCode());
-                    System.out.println("CANCELED: ErrorDetails=" + cancellation.getErrorDetails());
-                    System.out.println("CANCELED: Did you update the subscription info?");
-                }
-            }
-            
-            System.exit(exitCode);
-        } catch (Exception ex) {
-            System.out.println("Unexpected exception: " + ex.getMessage());
-
-            assert(false);
-            System.exit(1);
+        if (speechRecognitionResult.getReason() == ResultReason.RecognizedSpeech) {
+            System.out.println("RECOGNIZED: Text=" + speechRecognitionResult.getText());
         }
+        else if (speechRecognitionResult.getReason() == ResultReason.NoMatch) {
+            System.out.println("NOMATCH: Speech could not be recognized.");
+        }
+        else if (speechRecognitionResult.getReason() == ResultReason.Canceled) {
+            CancellationDetails cancellation = CancellationDetails.fromResult(speechRecognitionResult);
+            System.out.println("CANCELED: Reason=" + cancellation.getReason());
+
+            if (cancellation.getReason() == CancellationReason.Error) {
+                System.out.println("CANCELED: ErrorCode=" + cancellation.getErrorCode());
+                System.out.println("CANCELED: ErrorDetails=" + cancellation.getErrorDetails());
+                System.out.println("CANCELED: Did you set the speech resource key and region values?");
+            }
+        }
+
+        System.exit(0);
     }
 }
 // </code>
--- quickstart/java/jre/text-to-speech/src/speechsdk/quickstart/Main.java
+++ quickstart/java/jre/text-to-speech/src/speechsdk/quickstart/Main.java
@@ -3,74 +3,48 @@
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
 // <code>
-package speechsdk.quickstart;
+import com.microsoft.cognitiveservices.speech.*;
+import com.microsoft.cognitiveservices.speech.audio.*;
 
 import java.util.Scanner;
-import java.util.concurrent.Future;
-import com.microsoft.cognitiveservices.speech.*;
+import java.util.concurrent.ExecutionException;
 
-/**
- * Quickstart: synthesize speech using the Speech SDK for Java.
- */
-public class Main {
+public class SpeechSynthesis {
+    private static String YourSubscriptionKey = "YourSubscriptionKey";
+    private static String YourServiceRegion = "YourServiceRegion";
 
-    /**
-     * @param args Arguments are ignored in this sample.
-     */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        SpeechConfig speechConfig = SpeechConfig.fromSubscription(YourSubscriptionKey, YourServiceRegion);
 
-        // Replace below with your own subscription key
-        String speechSubscriptionKey = "YourSubscriptionKey";
-        // Replace below with your own service region (e.g., "westus").
-        String serviceRegion = "YourServiceRegion";
+        speechConfig.setSpeechSynthesisVoiceName("en-US-JennyNeural"); 
 
-        // Creates an instance of a speech synthesizer using speech configuration with
-        // specified
-        // subscription key and service region and default speaker as audio output.
-        try (SpeechConfig config = SpeechConfig.fromSubscription(speechSubscriptionKey, serviceRegion)) {
-            // Set the voice name, refer to https://aka.ms/speech/voices/neural for full
-            // list.
-            config.setSpeechSynthesisVoiceName("en-US-AriaNeural");
-            try (SpeechSynthesizer synth = new SpeechSynthesizer(config)) {
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig);
 
-                assert (config != null);
-                assert (synth != null);
-
-                int exitCode = 1;
-
-                System.out.println("Type some text that you want to speak...");
-                System.out.print("> ");
-                String text = new Scanner(System.in).nextLine();
-
-                Future<SpeechSynthesisResult> task = synth.SpeakTextAsync(text);
-                assert (task != null);
-
-                SpeechSynthesisResult result = task.get();
-                assert (result != null);
-
-                if (result.getReason() == ResultReason.SynthesizingAudioCompleted) {
-                    System.out.println("Speech synthesized to speaker for text [" + text + "]");
-                    exitCode = 0;
-                } else if (result.getReason() == ResultReason.Canceled) {
-                    SpeechSynthesisCancellationDetails cancellation = SpeechSynthesisCancellationDetails
-                            .fromResult(result);
-                    System.out.println("CANCELED: Reason=" + cancellation.getReason());
-
-                    if (cancellation.getReason() == CancellationReason.Error) {
-                        System.out.println("CANCELED: ErrorCode=" + cancellation.getErrorCode());
-                        System.out.println("CANCELED: ErrorDetails=" + cancellation.getErrorDetails());
-                        System.out.println("CANCELED: Did you update the subscription info?");
-                    }
-                }
-
-                System.exit(exitCode);
-            }
-        } catch (Exception ex) {
-            System.out.println("Unexpected exception: " + ex.getMessage());
-
-            assert (false);
-            System.exit(1);
+        // Get text from the console and synthesize to the default speaker.
+        System.out.println("Enter some text that you want to speak >");
+        String text = new Scanner(System.in).nextLine();
+        if (text.isEmpty())
+        {
+            return;
         }
+
+        SpeechSynthesisResult speechRecognitionResult = speechSynthesizer.SpeakTextAsync(text).get();
+
+        if (speechRecognitionResult.getReason() == ResultReason.SynthesizingAudioCompleted) {
+            System.out.println("Speech synthesized to speaker for text [" + text + "]");
+        }
+        else if (speechRecognitionResult.getReason() == ResultReason.Canceled) {
+            SpeechSynthesisCancellationDetails cancellation = SpeechSynthesisCancellationDetails.fromResult(speechRecognitionResult);
+            System.out.println("CANCELED: Reason=" + cancellation.getReason());
+
+            if (cancellation.getReason() == CancellationReason.Error) {
+                System.out.println("CANCELED: ErrorCode=" + cancellation.getErrorCode());
+                System.out.println("CANCELED: ErrorDetails=" + cancellation.getErrorDetails());
+                System.out.println("CANCELED: Did you set the speech resource key and region values?");
+            }
+        }
+
+        System.exit(0);
     }
 }
 // </code>
