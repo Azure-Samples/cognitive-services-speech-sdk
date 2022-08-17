68f650c9499ef6c0d12cc1385baed3121b8506fa
BrianMouncer <48774897+BrianMouncer@users.noreply.github.com>
2022-08-17 00:42:27 -0700

    Brianem/add python sample (#1617)
    * add python sample for start_continuous_recognition_async()
    
    * pull over more PR feedback on sample.
    
    * remove white space from emtpy line.
    
    * add new speech_recognize_continuous_async_from_microphone to main liast of samples.

--- samples/python/console/speech_sample.py
+++ samples/python/console/speech_sample.py
@@ -357,6 +357,54 @@ def speech_recognize_continuous_from_file():
     # </SpeechContinuousRecognitionWithFile>
 
 
+def speech_recognize_continuous_async_from_microphone():
+    """performs continuous speech recognition asynchronously with input from microphone"""
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+    # The default language is "en-us".
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
+
+    done = False
+
+    def recognizing_cb(evt: speechsdk.SpeechRecognitionEventArgs):
+        print('RECOGNIZING: {}'.format(evt))
+
+    def recognized_cb(evt: speechsdk.SpeechRecognitionEventArgs):
+        print('RECOGNIZED: {}'.format(evt))
+
+    def stop_cb(evt: speechsdk.SessionEventArgs):
+        """callback that signals to stop continuous recognition"""
+        print('CLOSING on {}'.format(evt))
+        nonlocal done
+        done = True
+
+    # Connect callbacks to the events fired by the speech recognizer
+    speech_recognizer.recognizing.connect(recognizing_cb)
+    speech_recognizer.recognized.connect(recognized_cb)
+    speech_recognizer.session_stopped.connect(stop_cb)
+    speech_recognizer.canceled.connect(stop_cb)
+
+    # Perform recognition. `start_continuous_recognition_async asynchronously initiates continuous recognition operation,
+    # Other tasks can be performed on this thread while recognition starts...
+    # wait on result_future.get() to know when initialization is done.
+    # Call stop_continuous_recognition_async() to stop recognition.
+    result_future = speech_recognizer.start_continuous_recognition_async()
+
+    result_future.get()  # wait for voidfuture, so we know engine initialization is done.
+    print('Continuous Recognition is now running, say something.')
+
+    while not done:
+        # No real sample parallel work to do on this thread, so just wait for user to type stop.
+        # Can't exit function or speech_recognizer will go out of scope and be destroyed while running.
+        print('type "stop" then enter when done')
+        stop = input()
+        if (stop.lower() == "stop"):
+            print('Stopping async recognition.')
+            speech_recognizer.stop_continuous_recognition_async()
+            break
+
+    print("recognition stopped, main thread can exit now.")
+
+
 # <SpeechRecognitionUsingKeywordModel>
 def speech_recognize_keyword_from_microphone():
     """performs keyword-triggered speech recognition with input microphone"""
@@ -492,7 +540,7 @@ def push_stream_writer(stream):
     wav_fh = wave.open(weatherfilename)
     # start pushing data until all data has been read from the file
     try:
-        while(True):
+        while True:
             frames = wav_fh.readframes(n_bytes // 2)
             print('read {} bytes'.format(len(frames)))
             if not frames:
