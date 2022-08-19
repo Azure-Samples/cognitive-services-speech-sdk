dce8a60f86545619f133c8a9dde36a22b517f06b
Henry van der Vegte <jvandervegte@microsoft.com>
2022-08-19 14:49:46 +0200

    add transientFailureException and retry in this case

--- samples/ingestion/ingestion-client/FetchTranscription/TextAnalytics/TextAnalyticsProvider.cs
+++ samples/ingestion/ingestion-client/FetchTranscription/TextAnalytics/TextAnalyticsProvider.cs
@@ -336,9 +336,9 @@ namespace TextAnalytics
                 var operation = await TextAnalyticsClient.StartAnalyzeActionsAsync(documentChunk, actions, cancellationToken: cts.Token).ConfigureAwait(false);
                 return (operation.Id, errors);
             }
-            catch (OperationCanceledException)
+            catch (OperationCanceledException operationCanceledException)
             {
-                throw new TimeoutException($"The operation has timed out after {RequestTimeout.TotalSeconds} seconds.");
+                throw new TransientFailureException($"Operation was canceled after {RequestTimeout.TotalSeconds} seconds.", operationCanceledException);
             }
 
             // do not catch throttling errors, rather throw and retry
@@ -430,9 +430,9 @@ namespace TextAnalytics
                     }
                 }
             }
-            catch (OperationCanceledException)
+            catch (OperationCanceledException operationCanceledException)
             {
-                throw new TimeoutException($"The operation has timed out after {RequestTimeout.TotalSeconds} seconds.");
+                throw new TransientFailureException($"Operation was canceled after {RequestTimeout.TotalSeconds} seconds.", operationCanceledException);
             }
 
             // do not catch throttling errors, rather throw and retry
