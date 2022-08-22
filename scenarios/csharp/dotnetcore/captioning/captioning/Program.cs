3254123dc5096158a44ef24d2732f3213a4a5cea
v-jaswel <v-jasonwells@microsoft.com>
2022-08-22 07:20:30 -0700

    Fix WebVTT formatting.

--- scenarios/csharp/dotnetcore/captioning/captioning/Program.cs
+++ scenarios/csharp/dotnetcore/captioning/captioning/Program.cs
@@ -185,7 +185,8 @@ namespace Captioning
                 caption.AppendFormat($"{this._srtSequenceNumber}{Environment.NewLine}");
                 this._srtSequenceNumber++;
             }
-            caption.AppendFormat($"{GetTimestamp(startTime, endTime)}{Environment.NewLine}");
+            // TODO1 This is for WebVTT. Need to figure out SRT also.
+            caption.AppendFormat($"{GetTimestamp(startTime, endTime)} line:-{this._userConfig.maxCaptionLines} align:start{Environment.NewLine}");
             if (null != language)
             {
                 caption.Append($"[{language}] ");
@@ -203,11 +204,14 @@ namespace Captioning
             // That way, captions start at (maxCaptionLines) from the bottom of the screen,
             // and progress toward the bottom of the screen, rather than start at the
             // bottom and be pushed upward.
+// TODO1 Remove
+/*
             var captionLines = Regex.Matches(caption.Text, "\\n").Count + 1;
             for (int i = 0; i < this._userConfig.maxCaptionLines - captionLines; i++)
             {
                 caption.Text += "\n&nbsp;";
             }
+*/
             
             // If the current caption has decreased in size, due to a change in a previous caption,
             // drop this caption and do not update the previous ending timestamp.
