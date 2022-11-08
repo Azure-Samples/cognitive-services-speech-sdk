ca3a0bbfa38204a30ce5989eda00b34015f897b1
Yulin Li <liyulin@pku.edu.cn>
2022-11-08 15:38:35 +0800

    batch synthesis csharp sample (#1731)
    * Batch synthesis sample
    
    * Fix typos  and deleted secrets which has been revoked now
    
    * Fix typo
    
    * Refine wording
    
    * Rename folder
    
    * Refine folder structure
    
    Co-authored-by: Ling Cao <caoling@microsoft.com>

---
+++ samples/batch-synthesis/csharp/BatchSynthesisSample/Program.cs
@@ -0,0 +1,54 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
+// The host name of the batch sytnhesis API, e.g. https://eastus.customvoice.api.speech.microsoft.com
+var hostName = "https://centraluseuap.customvoice.api.speech.microsoft.com";
+var subscriptionKey = "b410a73b5ded4a799bdbfdb7c83a459d";
+
+var sampleScript = "This is the waistline, and it's falling.\r\nAlexis, meet Bill and Hillary, and the rest of America.\r\nThis is Jordan, Scottie Pippen and the ring dynasty.\r\nThe more I looked, the gloomier I got.\r\nHe saw Macbeth, the three witches, and the boiling cauldron.\r\nBut it is good to get together, cook together, eat together and enjoy.\r\nThe priorities, they say, are sofa, blanket, and clothing.\r\nThe metallic walls curve, twist, and turn.\r\nThe seagrass fiber is tough, durable and smooth.\r\nThe dissenters were Stevens, Souter, Ginsburg and Breyer.";
+
+var synthesisClient = new BatchSynthesisClient(hostName, subscriptionKey);
+
+// Get all synthesis tasks.
+var syntheses = await synthesisClient.GetAllSynthesesAsync().ConfigureAwait(false);
+
+// Create a new synthesis task with plain text
+var newSynthesisUri = await synthesisClient.CreateSynthesisAsync(
+    "en-US-JennyNeural",
+    "sample batch synthesis",
+    "sample description",
+    sampleScript,
+    false).ConfigureAwait(false);
+
+var newSynthesisId = Guid.Parse(newSynthesisUri.Segments.Last());
+
+// Get a synthesis task.
+var synthesis = await synthesisClient.GetSynthesisAsync(newSynthesisId).ConfigureAwait(false);
+
+// Poll the synthesis until it completes
+var terminatedStates = new[] { "Succeeded", "Failed" };
+while (!terminatedStates.Contains(synthesis.Status))
+{
+    Console.WriteLine($"Synthesis {newSynthesisId}. Status: {synthesis.Status}");
+    await Task.Delay(TimeSpan.FromSeconds(30)).ConfigureAwait(false);
+    synthesis = await synthesisClient.GetSynthesisAsync(newSynthesisId).ConfigureAwait(false);
+}
+
+// Get outputs of the synthesis
+if (!string.IsNullOrEmpty(synthesis.Outputs.Result))
+{
+    Console.WriteLine("Please download result from this URL before you delete the synthesis.");
+    Console.WriteLine(synthesis.Outputs.Result);
+}
+
+if (!string.IsNullOrEmpty(synthesis.Outputs.Summary))
+{
+    Console.WriteLine("Please download summary file from this URL before you delete the synthesis.");
+    Console.WriteLine(synthesis.Outputs.Summary);
+}
+
+// Delete a specific synthesis
+await synthesisClient.DeleteSynthesisAsync(newSynthesisId);
+Console.WriteLine($"Deleted synthesis {newSynthesisId}.");
---
+++ samples/batch-synthesis/csharp/BatchSynthesisSample/dto/BatchSynthesisPropertiesDefinition.cs
@@ -0,0 +1,38 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
+public class BatchSynthesisPropertiesDefinition
+{
+    /// <summary>
+    /// The requested audio output format.
+    /// </summary>
+    public string OutputFormat { get; set; }
+
+    /// <summary>
+    /// The value indicating whether synthesis result is concatenated.
+    /// </summary>
+    public bool ConcatenateResult { get; set; }
+
+    /// <summary>
+    /// The requested destination container. It is an URL with Service adhoc SAS to a writeable container in Azure Blob storage.
+    /// </summary>
+    public Uri DestinationContainerUrl { get; set; }
+
+    /// <summary>
+    /// The value indicating whether synthesis result is decopressed in target container.
+    /// It only take effect when "destinationContainerUrl" is specified or "BYOS" is configured.
+    /// </summary>
+    public bool DecompressOutputFiles { get; set; }
+
+    /// <summary>
+    /// The value indicating whether the word boundary output will be generated.
+    /// </summary>
+    public bool WordBoundaryEnabled { get; set; }
+
+    /// <summary>
+    /// The value indicating whether the sentence boundary output will be generated.
+    /// </summary>
+    public bool SentenceBoundaryEnabled { get; set; }
+}
