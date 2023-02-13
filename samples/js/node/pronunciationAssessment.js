44132a3252958de076a72312e6fddc3074289dbc
v-liukun <v-liukun@microsoft.com>
2023-02-13 15:18:10 +0800

    Add comment

--- samples/js/node/pronunciationAssessment.js
+++ samples/js/node/pronunciationAssessment.js
@@ -118,7 +118,9 @@ module.exports = {
             _.filter(wholelyricsArry, (item) => !!item),
             (item) => item.trim()
         );
-
+        
+        // For continuous pronunciation assessment mode, the service won't return the words with `Insertion` or `Omission`
+        // We need to compare with the reference text after received all recognized words to get these error words.
         const diff = new difflib.SequenceMatcher(null, wholelyricsArryRes, resTextArray);
         const lastWords = [];
         for (const d of diff.getOpcodes()) {
