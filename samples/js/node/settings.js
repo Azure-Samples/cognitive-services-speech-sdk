cbb1456795edb48303f2ac9fe499f60d590cc72f
glenn <glharper@microsoft.com>
2023-11-09 17:26:22 -0500

    indent on settings

--- samples/js/node/settings.js
+++ samples/js/node/settings.js
@@ -1,19 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-  
-  // Replace with your own subscription key, service region (e.g., "westus"),
-  // and recognition language.
-  export const subscriptionKey =   "YourSubscriptionKey";
-  export const serviceRegion =     "YourSubscriptionRegion"; // e.g., "westus"
-  export const language =          "en-US";
+// Replace with your own subscription key, service region (e.g., "westus"),
+// and recognition language.
+export const subscriptionKey =   "YourSubscriptionKey";
+export const serviceRegion =     "YourSubscriptionRegion"; // e.g., "westus"
+export const language =          "en-US";
 
-  // Replace with the full path to a wav file you want to recognize or overwrite.
-  export const filename =          "YourAudioFile.wav"; // 16000 Hz, Mono
+// Replace with the full path to a wav file you want to recognize or overwrite.
+export const filename =          "YourAudioFile.wav"; // 16000 Hz, Mono
 
-  // Replace with your own Language Understanding subscription key (endpoint
-  // key), region, and app ID in case you want to run the intent sample.
-  export const luSubscriptionKey = "YourLanguageUnderstandingSubscriptionKey";
-  export const luServiceRegion =   "YourLanguageUnderstandingServiceRegion";
-  export const luAppId =           "YourLanguageUnderstandingAppId";
-  
+// Replace with your own Language Understanding subscription key (endpoint
+// key), region, and app ID in case you want to run the intent sample.
+export const luSubscriptionKey = "YourLanguageUnderstandingSubscriptionKey";
+export const luServiceRegion =   "YourLanguageUnderstandingServiceRegion";
+export const luAppId =           "YourLanguageUnderstandingAppId";
+
