93872ce046f78bda2646c0c017ba1393e58b4f30
ForrestGumb <ForrestGumb@hotmail.com>
2024-01-12 19:25:36 +0800

    Add sample code for Custom Voice REST API (#2211)
    Co-authored-by: Jun-wei Gan <junwg@microsoft.com>

---
+++ samples/custom-voice/python/customvoice/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+
+from .config import *
+from .customvoice_object import *
+from .status_object import *
+from .project import *
+from .consent import *
+from .training_set import *
+from .model import *
+from .endpoint import *
+from .personal_voice import *
