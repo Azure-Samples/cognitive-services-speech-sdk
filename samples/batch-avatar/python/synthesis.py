745f6a9e075f454632eab5a2ce52aed38a32a4e8
Yulin Li <yulili@microsoft.com>
2024-01-26 14:53:09 +0800

    [batch-avatar] update sameple with new API version `2024-04-01-preview`

--- samples/batch-avatar/python/synthesis.py
+++ samples/batch-avatar/python/synthesis.py
@@ -9,38 +9,55 @@ import logging
 import os
 import sys
 import time
-from pathlib import Path
+import uuid
 
+from azure.identity import DefaultAzureCredential
 import requests
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,  # set to logging.DEBUG for verbose output
         format="[%(asctime)s] %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p %Z")
 logger = logging.getLogger(__name__)
 
-# Your Speech resource key and region
-# This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
 
-SUBSCRIPTION_KEY = os.environ.get('SPEECH_KEY')
-SERVICE_REGION = os.environ.get('SPEECH_REGION')
-
-NAME = "Simple avatar synthesis"
-DESCRIPTION = "Simple avatar synthesis description"
-
-# The service host suffix.
-SERVICE_HOST = "customvoice.api.speech.microsoft.com"
+# The endpoint (and key) could be gotten from the Keys and Endpoint page in the Speech service resource.
+# The endpoint would be like: https://<region>.api.cognitive.microsoft.com or https://<custom_domain>.cognitiveservices.azure.com
+SPEECH_ENDPOINT = os.environ.get('SPEECH_ENDPOINT')
+# We recommend to use passwordless authentication with Azure Identity here; meanwhile, you can also use a subscription key instead
+PASSWORDLESS_AUTHENTICATION = True
+API_VERSION = "2024-04-01-preview"
 
 
-def submit_synthesis():
-    url = f'https://{SERVICE_REGION}.{SERVICE_HOST}/api/texttospeech/3.1-preview1/batchsynthesis/talkingavatar'
+def _create_job_id():
+    # the job ID must be unique in current speech resource
+    # you can use a GUID or a self-increasing number
+    return uuid.uuid4()
+
+
+def _authenticate():
+    if PASSWORDLESS_AUTHENTICATION:
+        # Refer to https://learn.microsoft.com/python/api/overview/azure/identity-readme?view=azure-python#defaultazurecredential
+        # for more information about Azure Identity
+        # For example, your app can authenticate using your Azure CLI sign-in credentials with when developing locally.
+        # Your app can then use a managed identity once it has been deployed to Azure. No code changes are required for this transition.
+
+        # When developing locally, make sure that the user account that is accessing batch avatar synthesis has the right permission.
+        # You'll need Storage Blob Data Contributor to read and write blob data.
+        credential = DefaultAzureCredential()
+        token = credential.get_token('https://cognitiveservices.azure.com/.default')
+        return {'Authorization': f'Bearer {token.token}'}
+    else:
+        SUBSCRIPTION_KEY = os.environ.get('SPEECH_KEY')
+        return {'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY}
+
+
+def submit_synthesis(job_id: str):
+    url = f'{SPEECH_ENDPOINT}/avatar/batchsyntheses/{job_id}?api-version={API_VERSION}'
     header = {
-        'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY,
         'Content-Type': 'application/json'
     }
+    header.update(_authenticate())
 
     payload = {
-        'displayName': NAME,
-        'description': DESCRIPTION,
-        "textType": "PlainText",
         'synthesisConfig': {
             "voice": "en-US-JennyNeural",
         },
@@ -50,12 +67,13 @@ def submit_synthesis():
         'customVoices': {
             # "YOUR_CUSTOM_VOICE_NAME": "YOUR_CUSTOM_VOICE_ID"
         },
+        "inputKind": "plainText",
         "inputs": [
             {
-                "text": "Hi, I'm a virtual assistant created by Microsoft.",
+                "content": "Hi, I'm a virtual assistant created by Microsoft.",
             },
         ],
-        "properties": {
+        "avatarConfig": {
             "customized": False, # set to True if you want to use customized avatar
             "talkingAvatarCharacter": "lisa",  # talking avatar character
             "talkingAvatarStyle": "graceful-sitting",  # talking avatar style, required for prebuilt avatar, optional for custom avatar
@@ -63,23 +81,23 @@ def submit_synthesis():
             "videoCodec": "h264",  # hevc, h264 or vp9, vp9 is required for transparent background; default is hevc
             "subtitleType": "soft_embedded",
             "backgroundColor": "#FFFFFFFF", # background color in RGBA format, default is white; can be set to 'transparent' for transparent background
+            # "backgroundImage": "https://samples-files.com/samples/Images/jpg/1920-1080-sample.jpg", # background image URL, only support https, either backgroundImage or backgroundColor can be set
         }
     }
 
-    response = requests.post(url, json.dumps(payload), headers=header)
+    response = requests.put(url, json.dumps(payload), headers=header)
     if response.status_code < 400:
         logger.info('Batch avatar synthesis job submitted successfully')
         logger.info(f'Job ID: {response.json()["id"]}')
-        return response.json()["id"]
+        return True
     else:
         logger.error(f'Failed to submit batch avatar synthesis job: {response.text}')
 
 
 def get_synthesis(job_id):
-    url = f'https://{SERVICE_REGION}.{SERVICE_HOST}/api/texttospeech/3.1-preview1/batchsynthesis/talkingavatar/{job_id}'
-    header = {
-        'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY
-    }
+    url = f'{SPEECH_ENDPOINT}/avatar/batchsyntheses/{job_id}?api-version={API_VERSION}'
+    header = _authenticate()
+
     response = requests.get(url, headers=header)
     if response.status_code < 400:
         logger.debug('Get batch synthesis job successfully')
@@ -93,10 +111,9 @@ def get_synthesis(job_id):
 
 def list_synthesis_jobs(skip: int = 0, top: int = 100):
     """List all batch synthesis jobs in the subscription"""
-    url = f'https://{SERVICE_REGION}.{SERVICE_HOST}/api/texttospeech/3.1-preview1/batchsynthesis/talkingavatar?skip={skip}&top={top}'
-    header = {
-        'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY
-    }
+    url = f'{SPEECH_ENDPOINT}/avatar/batchsyntheses?api-version={API_VERSION}&$skip={skip}&$top={top}'
+    header = _authenticate()
+
     response = requests.get(url, headers=header)
     if response.status_code < 400:
         logger.info(f'List batch synthesis jobs successfully, got {len(response.json()["values"])} jobs')
@@ -106,8 +123,8 @@ def list_synthesis_jobs(skip: int = 0, top: int = 100):
 
 
 if __name__ == '__main__':
-    job_id = submit_synthesis()
-    if job_id is not None:
+    job_id = _create_job_id()
+    if submit_synthesis(job_id):
         while True:
             status = get_synthesis(job_id)
             if status == 'Succeeded':
