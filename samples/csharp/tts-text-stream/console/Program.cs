95c8175f58d1e45181626bfe126566c5aea27030
Zheng Niu <zhn@microsoft.com>
2024-04-25 16:44:25 +0800

    Fix path

--- samples/csharp/tts-text-stream/console/Program.cs
+++ samples/csharp/tts-text-stream/console/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace Demo
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
+namespace Demo
 {
     using Azure;
     using Azure.AI.OpenAI;
