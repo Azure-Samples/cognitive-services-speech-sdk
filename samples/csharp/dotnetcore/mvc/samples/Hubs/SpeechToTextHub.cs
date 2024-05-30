28847560cc72100c7e9c01ce02432678b6c1b665
Mike Alhayek <mike@crestapps.com>
2024-05-30 14:41:41 -0700

    Fix the continuous recognition

--- samples/csharp/dotnetcore/mvc/samples/Hubs/SpeechToTextHub.cs
+++ samples/csharp/dotnetcore/mvc/samples/Hubs/SpeechToTextHub.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Concentus;
+using Concentus.Oggfile;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
 using System.Text;
@@ -20,7 +22,7 @@ public sealed class SpeechToTextHub : Hub<ISpeechToTextHub>
         _configuration = configuration;
     }
 
-    public async Task ContinuousRecognition(IAsyncEnumerable<string> stream)
+    public async Task ContinuousRecognition(IAsyncEnumerable<string> stream, uint samplePerSecond, byte channels)
     {
         var speechConfig = _configuration.GetSpeechConfig();
 
@@ -31,7 +33,8 @@ public sealed class SpeechToTextHub : Hub<ISpeechToTextHub>
             return;
         }
 
-        using var format = AudioStreamFormat.GetCompressedFormat(AudioStreamContainerFormat.OGG_OPUS);
+        using var format = AudioStreamFormat.GetWaveFormatPCM(samplePerSecond, 16, channels);
+
         using var audioInputStream = AudioInputStream.CreatePushStream(format);
         using var audioConfig = AudioConfig.FromStreamInput(audioInputStream);
         var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
@@ -43,7 +46,7 @@ public sealed class SpeechToTextHub : Hub<ISpeechToTextHub>
 
         await recognizer.StartContinuousRecognitionAsync();
 
-        await PushAudioAsync(audioInputStream, stream);
+        await PushAudioAsync(audioInputStream, stream, (int)samplePerSecond, channels);
 
         await recognizer.StopContinuousRecognitionAsync();
 
@@ -66,17 +69,17 @@ public sealed class SpeechToTextHub : Hub<ISpeechToTextHub>
             return;
         }
 
+        await Clients.Caller.SessionStarted();
+
         var format = AudioStreamFormat.GetCompressedFormat(AudioStreamContainerFormat.OGG_OPUS);
         var audioInputStream = AudioInputStream.CreatePushStream(format);
 
-        await PushAudioAsync(audioInputStream, stream);
+        await PopulateAudioAsync(audioInputStream, stream);
 
         try
         {
             using var audioConfig = AudioConfig.FromStreamInput(audioInputStream);
             using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
-
-            await Clients.Caller.ReceiveMessage("Session Started.");
 
             var result = await recognizer.RecognizeOnceAsync();
 
@@ -87,7 +90,6 @@ public sealed class SpeechToTextHub : Hub<ISpeechToTextHub>
             else if (result.Reason == ResultReason.NoMatch)
             {
                 await Clients.Caller.ReceiveMessage("Speech could not be recognized. Please speak clearly into the microphone.");
-                await Clients.Caller.ReceiveMessage("Session Ended.");
             }
             else
             {
@@ -104,50 +106,83 @@ public sealed class SpeechToTextHub : Hub<ISpeechToTextHub>
                     await Clients.Caller.ReceiveMessage($"Failed to recognize the incoming speech. Reason {cancellation.Reason}.", true);
                 }
             }
-
-            await Clients.Caller.ReceiveMessage("Session Ended.");
         }
         finally
         {
             audioInputStream.Dispose();
+            await Clients.Caller.SessionStopped();
         }
     }
 
-    private static async Task PushAudioAsync(PushAudioInputStream audioInputStream, IAsyncEnumerable<string> stream)
+    private static async Task PushAudioAsync(PushAudioInputStream audioInputStream, IAsyncEnumerable<string> stream, int sampleRate, int channels)
     {
+        var decoder = OpusCodecFactory.CreateDecoder(sampleRate, channels);
+
         await foreach (var base64String in stream)
         {
             using var memoryStream = new MemoryStream();
 
             var chunk = Convert.FromBase64String(base64String);
             memoryStream.Write(chunk, 0, chunk.Length);
+
+            // Reset the position of the stream.
             memoryStream.Seek(0, SeekOrigin.Begin);
 
-            using var binaryReader = new BinaryReader(memoryStream, Encoding.UTF8, leaveOpen: true);
-            byte[] readBytes;
-            do
+            var oggStream = new OpusOggReadStream(decoder, memoryStream);
+
+            while (oggStream.HasNextPacket)
             {
-                readBytes = binaryReader.ReadBytes(1024);
-                audioInputStream.Write(readBytes);
-            } while (readBytes.Length > 0);
+                // Decode the next packet.
+                short[] packet = oggStream.DecodeNextPacket();
+                if (packet == null)
+                {
+                    continue;
+                }
+
+                for (int i = 0; i < packet.Length; i++)
+                {
+                    var bytes = BitConverter.GetBytes(packet[i]);
+                    audioInputStream.Write(bytes, bytes.Length);
+                }
+            }
         }
+    }
+
+    private static async Task PopulateAudioAsync(PushAudioInputStream audioInputStream, IAsyncEnumerable<string> stream)
+    {
+        using var memoryStream = new MemoryStream();
+
+        await foreach (var base64String in stream)
+        {
+            var chunk = Convert.FromBase64String(base64String);
+            memoryStream.Write(chunk, 0, chunk.Length);
+        }
+        memoryStream.Seek(0, SeekOrigin.Begin);
+
+        using var binaryReader = new BinaryReader(memoryStream, Encoding.UTF8, leaveOpen: true);
+        byte[] readBytes;
+        do
+        {
+            readBytes = binaryReader.ReadBytes(1024);
+            audioInputStream.Write(readBytes);
+        } while (readBytes.Length > 0);
     }
 
     private void RecognizerStopped(object sender, SessionEventArgs e)
     {
-        Clients.Caller.ReceiveMessage("Session Ended.").GetAwaiter().GetResult();
+        Clients.Caller.SessionStopped();
     }
 
     private void RecognizerStarted(object sender, SessionEventArgs e)
     {
-        Clients.Caller.ReceiveMessage("Session Started.").GetAwaiter().GetResult();
+        Clients.Caller.SessionStarted();
     }
 
     private void RecognizerRecognizing(object sender, SpeechRecognitionEventArgs e)
     {
         _logger.LogDebug("RecognizerRecognizing. Result: {Result}. Reason {Reason}", e.Result?.Text, e.Result?.Reason);
 
-        if (e.Result.Reason == ResultReason.RecognizedSpeech)
+        if (e.Result.Reason == ResultReason.RecognizingSpeech)
         {
             Clients.Caller.ReceivePartialMessage(e.Result.Text).GetAwaiter().GetResult();
         }
@@ -163,7 +198,7 @@ public sealed class SpeechToTextHub : Hub<ISpeechToTextHub>
 
         if (e.Result.Reason == ResultReason.RecognizedSpeech)
         {
-            Clients.Caller.ReceiveMessage("Final Result: " + e.Result.Text).GetAwaiter().GetResult();
+            Clients.Caller.ReceiveMessage(e.Result.Text).GetAwaiter().GetResult();
         }
         else if (e.Result.Reason == ResultReason.NoMatch)
         {
@@ -188,4 +223,8 @@ public interface ISpeechToTextHub
     Task ReceiveMessage(string message, bool isError = false);
 
     Task ReceivePartialMessage(string message);
+
+    Task SessionStopped();
+
+    Task SessionStarted();
 }
