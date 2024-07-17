868c858264a84afbde43db0728dfa8ff254c3846
Komal Grover <komalgrover@microsoft.com>
2024-07-17 16:08:18 +0000

    updating failing test

--- samples/ingestion/ingestion-client/Tests/EndToEndTests.cs
+++ samples/ingestion/ingestion-client/Tests/EndToEndTests.cs
@@ -52,9 +52,10 @@ namespace Tests
                     FallbackRole = Role.None,
                 }
             }));
-            var region = testContext.Properties["LanguageServiceRegion"].ToString();
-            var subscriptionKey = testContext.Properties["LanguageServiceSubscriptionKey"].ToString();
-            var provider = new AnalyzeConversationsProvider("en-US", subscriptionKey, region, Logger.Object);
+
+            // var region = testContext.Properties["LanguageServiceRegion"].ToString();
+            // var subscriptionKey = testContext.Properties["LanguageServiceSubscriptionKey"].ToString();
+            var provider = new AnalyzeConversationsProvider("en-US",  "sometestkey", "https://sometestendpoint", Logger.Object);
             var body = File.ReadAllText(@"TestFiles/summarizationInputSample.json");
             var transcription = JsonConvert.DeserializeObject<SpeechTranscript>(body);
 
