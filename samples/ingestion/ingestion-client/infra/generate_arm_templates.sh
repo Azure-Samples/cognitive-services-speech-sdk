669493623de62bd9b52b7fa0e77bf662c52db7f3
Henry van der Vegte <jvandervegte@microsoft.com>
2024-09-18 16:38:31 +0200

    Update generate_arm_templates.sh

--- samples/ingestion/ingestion-client/infra/generate_arm_templates.sh
+++ samples/ingestion/ingestion-client/infra/generate_arm_templates.sh
@@ -5,9 +5,7 @@ set -e
 BICEP_VERSION="v0.30.3"
 
 # if az bicep is not installed, install it else upgrade it
-if ! command -v az bicep &> /dev/null; then
-    az bicep install --version "$BICEP_VERSION"
-elif [ "$(az bicep version --query 'bicepVersion' -o tsv)" != "$BICEP_VERSION" ]; then
+if ! command -v az bicep &> /dev/null || [ "$(az bicep version --query 'bicepVersion' -o tsv)" != "$BICEP_VERSION" ]; then
     az bicep install --version "$BICEP_VERSION"
 fi
 
