27ea581f4a1544a33cb7399af01f943ee351bdb1
Shaoqing Tang (CSI Interfusion Inc) <v-shaota@microsoft.com>
2024-10-28 16:55:30 +0800

    Fix bugs in TranslateSpeechNetCore and SynthesizeSpeechPython when testing on Windows and Linux

--- quickstart/csharp/dotnetcore/translate-speech-to-text/build_run_app.sh
+++ quickstart/csharp/dotnetcore/translate-speech-to-text/build_run_app.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
+set -e  # Exit immediately if a command exits with a non-zero status.
+
+action="$1"
+
 # Check if the correct number of arguments is provided
 if [ "$#" -lt 1 ]; then
     echo "Usage: $0 {build|run}"
     exit 1
 fi
 
-action=$1
+dotnetInstallPath="/usr/share/dotnet"
+export PATH=$PATH:$dotnetInstallPath
 
 install_dotnet6() {
     wget https://dot.net/v1/dotnet-install.sh
     chmod +x dotnet-install.sh
-    ./dotnet-install.sh --version 6.0.427
+    sudo ./dotnet-install.sh --version 6.0.427 --install-dir $dotnetInstallPath
 
-    if [ $? -ne 0 ]; then
+    if [ $? -eq 0 ]; then
+        echo 'export PATH=$PATH:/usr/share/dotnet' >> ~/.bashrc
+        source ~/.bashrc
+        sudo rm -f ./dotnet-install.sh
+    else
         echo "Installation of .NET SDK 6.0 failed, exiting..."
+        sudo rm -f ./dotnet-install.sh
         exit 1
     fi
 }
@@ -26,6 +36,14 @@ if [ "$action" = "build" ]; then
     if ! command -v dotnet &> /dev/null || [[ "$(dotnet --version)" < "6.0" ]]; then
         echo "Installing .NET SDK 6.0..."
         install_dotnet6
+    fi
+
+    dotnet add ./helloworld package Microsoft.CognitiveServices.Speech --interactive
+    if [ $? -eq 0 ]; then
+        echo "Installation Microsoft.CognitiveServices.Speech package is succeeded."
+    else
+        echo "Installation Microsoft.CognitiveServices.Speech package is failed, exiting..."
+        exit 1
     fi
 
     dotnet build ./helloworld --configuration release
