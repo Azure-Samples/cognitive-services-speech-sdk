f7d060bc5c5ce9066aedc09ba7ac3f11e9e5e6e6
Shaoqing Tang (CSI Interfusion Inc) <v-shaota@microsoft.com>
2024-11-11 17:46:24 +0800

    Create quickstart translate speech java jre sample

--- quickstart/java/jre/translate-speech-to-text/README.md
+++ quickstart/java/jre/translate-speech-to-text/README.md
@@ -3,6 +3,17 @@
 This sample demonstrates how to translate speech with the Speech SDK for Java on Windows, macOS or Linux.
 See the [accompanying article](https://docs.microsoft.com/azure/cognitive-services/speech-service/get-started-speech-translation?tabs=script%2Cwindowsinstall&pivots=programming-language-java) on the SDK documentation page which describes how to build this sample from scratch in Eclipse.
 
+> **Note:**
+> The Speech SDK for the JRE currently supports the Windows x64 platform, macOS x64 (10.14 or later), macOS M1 arm64 (11.0 or later), and [specific Linux distributions and target architectures](https://docs.microsoft.com/azure/cognitive-services/speech-service/speech-sdk?tabs=linux).
+
+## Run the Sample within VS Code
+- Install "Azure AI Speech Toolkit" extension in VS Code.
+- Download this sample from sample gallery to local machine.
+- Trigger "Azure AI Speech Toolkit: Configure Azure Speech Resources" command from command palette to select speech resource.
+- Trigger "Azure AI Speech Toolkit: Configure and Setup the Sample App" command from command palette to configure and setup the sample. This command only needs to be run once.
+- Trigger "Azure AI Speech Toolkit: Build the Sample App" command from command palette to build the sample.
+- Trigger "Azure AI Speech Toolkit: Run the Sample App" command from command palette to run the sample.
+
 ## Prerequisites
 
 * A subscription key for the Speech service. See [Try the speech service for free](https://docs.microsoft.com/azure/cognitive-services/speech-service/get-started).
--- quickstart/java/jre/translate-speech-to-text/src/speechsdk/quickstart/Main.java
+++ quickstart/java/jre/translate-speech-to-text/src/speechsdk/quickstart/Main.java
@@ -5,7 +5,13 @@
 // <code>
 package speechsdk.quickstart;
 
+package speechsdk.quickstart;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.io.IOException;
+import java.nio.file.Path;
+import org.json.JSONObject;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
@@ -16,15 +22,19 @@ public class Main {
 
     public static void translationWithMicrophoneAsync() throws InterruptedException, ExecutionException, IOException
     {
+        Path configPath = Paths.get(System.getProperty("user.dir"), "config.json");
+        String content = new String(Files.readAllBytes(configPath));
+        JSONObject jsonObject = new JSONObject(content);
+
         // Replace with your own subscription key.
-        String speechSubscriptionKey = "YourSubscriptionKey";
+        String speechSubscriptionKey = jsonObject.getString("SubscriptionKey");
         // Replace below with your own service region (e.g., "westus").
-        String speechServiceRegion = "YourServiceRegion";
-        
+        String speechServiceRegion = jsonObject.getString("ServiceRegion");
+
         // Creates an instance of a translation recognizer using speech translation configuration with specified
         // subscription key and service region and microphone as default audio input.
         try (SpeechTranslationConfig config = SpeechTranslationConfig.fromSubscription(speechSubscriptionKey, speechServiceRegion)) {
-            
+
             // Sets source and target language(s).
             String fromLanguage = "en-US";
             config.setSpeechRecognitionLanguage(fromLanguage);
@@ -33,7 +43,7 @@ public class Main {
             // Sets voice name of synthesis output.
             String GermanVoice = "de-DE-AmalaNeural";
             config.setVoiceName(GermanVoice);
-            
+
             try (TranslationRecognizer recognizer = new TranslationRecognizer(config)) {
                 // Subscribes to events.
                 recognizer.recognizing.addEventListener((s, e) -> {
