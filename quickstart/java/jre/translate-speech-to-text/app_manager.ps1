e2783aefef7be3a3f0cda36c155d94509b3444c6
Shaoqing Tang (CSI Interfusion Inc) <v-shaota@microsoft.com>
2024-11-12 18:35:25 +0800

    Update code

--- quickstart/java/jre/translate-speech-to-text/app_manager.ps1
+++ quickstart/java/jre/translate-speech-to-text/app_manager.ps1
@@ -3,10 +3,40 @@ param (
 )
 
 $downloadPath = "$env:TEMP"
-$javaCmd = "java"
-$mvnCmd = "mvn"
-$javaInstallPath = Join-Path $env:LOCALAPPDATA "Java\jdk-11"
+$javaInstallPath = Join-Path $env:LOCALAPPDATA "Java"
 $mavenInstallPath = Join-Path $env:LOCALAPPDATA "Maven"
+
+if ($env:PROCESSOR_ARCHITECTURE -ne "AMD64") {
+    Write-Host "Your Windows is not 64-bit architecture. Please use a computer with a 64-bit architecture running Windows 10 or later." -ForegroundColor Red
+    exit 1
+}
+
+# Registry path: Visual C++ Redistributable installation information
+$regKeyPath = "HKLM:\SOFTWARE\Microsoft\VisualStudio\14.0\VC\Runtimes\X64"
+# Check if it is installed
+$installed = Get-ItemProperty -Path $regKeyPath | Select-Object "Installed"
+if (-not $installed) {
+    Write-Host "The Visual C++ 2015-2022 Redistributable is not installed. Downloading and installing..."
+
+    # Download the installer
+    $installerPath = "$env:TEMP\vc_redist_x64_installer.exe"
+    Invoke-WebRequest -Uri "https://aka.ms/vs/17/release/vc_redist.x64.exe" -OutFile $installerPath
+    if (-not $?) {
+        Write-Host "Failed to download vc_redist.x64, exiting..." -ForegroundColor Red
+        exit 1
+    }
+
+    # Install Redistributable (Use silent installation)
+    Start-Process -FilePath $installerPath -ArgumentList "/quiet", "/norestart" -Wait
+    if (-not $?) {
+        Write-Host "Failed to install Visual C++ 2015-2022 Redistributable, exiting..." -ForegroundColor Red
+        exit 1
+    }
+
+    Write-Host "The Visual C++ 2015-2022 Redistributable installation completed."
+
+    Remove-Item -Force $installerPath
+}
 
 # Function to download and extract a zip file
 function Get-And-Extract ($url, $destinationPath, $zipPrefix) {
@@ -32,39 +62,65 @@ function Get-And-Extract ($url, $destinationPath, $zipPrefix) {
 }
 
 function Install-OpenJDK {
-    # Check if Java is installed and if the version is at least 11
-    $javaVersion = & java -version 2>&1 | Select-String -Pattern 'version "(?<version>\d+\.\d+).*"' | ForEach-Object { $_.Matches[0].Groups["version"].Value }
-    if (-not $javaVersion -or [version]$javaVersion -lt [version]"11.0") {
-        Write-Output "Installing Java JDK 11..."
-        Get-And-Extract -url "https://download.oracle.com/java/11/latest/jdk-11_windows-x64_bin.zip" -destinationPath $javaInstallPath -zipName "jdk-11"
+    Get-And-Extract -url "https://aka.ms/download-jdk/microsoft-jdk-11.0.25-windows-x64.zip" -destinationPath $javaInstallPath -zipPrefix "microsoft-openjdk-11"
+    $javaExecutable = Get-ChildItem -Path $javaInstallPath -Recurse -Filter "java.exe" -ErrorAction SilentlyContinue
+    $javaHomePath = $javaExecutable.FullName -replace '\\bin\\java\.exe$', ''
 
-        $javaCmd = Join-Path $javaInstallPath "bin\java.exe"
-        # $env:JAVA_HOME = $javaInstallPath
-        # $env:Path += ";$javaInstallPath\bin"
-        Write-Output "Java JDK 11 installed to $javaInstallPath."
+    $currentJavaHome = [Environment]::GetEnvironmentVariable("JAVA_HOME", [EnvironmentVariableTarget]::User)
+    if ($currentJavaHome -ne $javaHomePath) {
+        [Environment]::SetEnvironmentVariable("JAVA_HOME", $javaHomePath, [EnvironmentVariableTarget]::User)
+        Write-Host "JAVA_HOME has been set as: $javaHomePath"
+    }
+
+    $currentUserPath = [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User)
+    $javaJDKPath = "$javaHomePath\bin"
+    if ($currentUserPath -notlike "*$javaJDKPath*") {
+        $newUserPathValue = "$currentUserPath;$javaJDKPath"
+        [System.Environment]::SetEnvironmentVariable("PATH", $newUserPathValue, [System.EnvironmentVariableTarget]::User)
+    }
+
+    Write-Output "Java JDK 11 installed to $javaInstallPath."
+}
+
+function Find-OpenJDK {
+    # Check if Java is installed and if the version is at least 11
+    if (-not (Get-Command java -ErrorAction SilentlyContinue)) {
+        Write-Output "Installing Java JDK 11..."
+        Install-OpenJDK
+    }
+    else {
+        $javaVersion = & java -version 2>&1 | Select-String -Pattern 'version "(\d+)\.(\d+)\.(\d+)"' | ForEach-Object { $_.Matches.Groups[1].Value }
+        if ($javaVersion -lt 11) {
+            Write-Host "The installed Java version is lower than 11, installing Microsoft Build of OpenJDK 11..."
+            Install-OpenJDK
+        }
     }
 }
 
 function Install-Maven {
     # Check if Maven is installed
-    $mavenVersion = & mvn -v 2>&1 | Select-String -Pattern 'Apache Maven (?<version>\d+\.\d+\.\d+)' | ForEach-Object { $_.Matches[0].Groups["version"].Value }
-    if (-not $mavenVersion) {
+    if (-not (Get-Command mvn -ErrorAction SilentlyContinue)) {
         Write-Output "Installing Maven..."
-        Get-And-Extract -url "https://archive.apache.org/dist/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.zip" -destinationPath $mavenInstallPath -zipName "apache-maven"
+        Get-And-Extract -url "https://archive.apache.org/dist/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.zip" -destinationPath $mavenInstallPath -zipPrefix "apache-maven"
 
-        $mvnCmd = Join-Path $mavenInstallPath "apache-maven-3.8.8\bin\mvn.cmd"
+        $currentUserPath = [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User)
+        $mvnCmd = Join-Path $mavenInstallPath "apache-maven-3.9.9\bin"
+        if ($currentUserPath -notlike "*$mvnCmd*") {
+            $newUserPathValue = "$currentUserPath;$mvnCmd"
+            [System.Environment]::SetEnvironmentVariable("PATH", $newUserPathValue, [System.EnvironmentVariableTarget]::User)
+        }
 
         Write-Output "Maven installed to $mavenInstallPath."
     }
 }
 
 if ($action -eq "configure") {
-    Install-OpenJDK
+    Find-OpenJDK
     Install-Maven
 
     # Install dependencies for the Java project in pom.xml
     Write-Output "Resolving Maven dependencies..."
-    & $mvnCmd dependency:resolve
+    & mvn dependency:resolve
     if (-not $?) {
         Write-Host "Java project dependencies installation failed." -BackgroundColor Red
         exit 1
@@ -72,7 +128,7 @@ if ($action -eq "configure") {
 }
 elseif ($action -eq "build") {
     Write-Host "Compiling Java files..."
-    & $mvnCmd compile
+    & mvn compile
     if (-not $?) {
         Write-Host "Java file compilation failed." -BackgroundColor Red
         exit 1
@@ -81,7 +137,7 @@ elseif ($action -eq "build") {
 }
 elseif ($action -eq "run") {
     Write-Host "Running Java application..."
-    & $mvnCmd exec:java -Dexec.mainClass="speechsdk.quickstart.Main"
+    & mvn exec:java -Dexec.mainClass="speechsdk.quickstart.Main"
 }
 else {
     Write-Host "Invalid action: $action"
