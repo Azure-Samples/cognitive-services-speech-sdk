382e22bbe0f04046630a566dd69b1280f9503f2d
poleli <poleli@microsoft.com>
2024-12-19 10:28:43 +0800

    Fix warning.

--- samples/video-translation/csharp/CommonLib.Public/HttpClient/ApimApiRegionConfig.cs
+++ samples/video-translation/csharp/CommonLib.Public/HttpClient/ApimApiRegionConfig.cs
@@ -1,4 +1,4 @@
-﻿//
+//
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
@@ -16,7 +16,6 @@ public class ApimApiRegionConfig : IRegionConfig
     }
 
     public string RegionIdentifier { get; private set; }
-
 
     public Uri EndpointUrl => new Uri($"https://{HostName}");
 
