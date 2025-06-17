29e0a2ce92cb2f62b6f8bca05958aba21cc85fd2
Dapeng Li <poleli@microsoft.com>
2025-06-17 15:18:08 +0800

    Fix bom.

--- samples/video-translation/csharp/VideoTranslationLib.Public/VideoTranslationLib.Public/DataContracts/DTOs/Public-2025-05-20/EventHubConfig.cs
+++ samples/video-translation/csharp/VideoTranslationLib.Public/VideoTranslationLib.Public/DataContracts/DTOs/Public-2025-05-20/EventHubConfig.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="EventHubConfig.cs" company="Microsoft Corporation">
+// <copyright file="EventHubConfig.cs" company="Microsoft Corporation">
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // </copyright>
 
