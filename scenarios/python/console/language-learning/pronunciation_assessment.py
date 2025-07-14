f4a4992c180c362d45f142b4d6f43810481cf4e1
Di Lin <lindisjtu@gmail.com>
2025-07-14 08:54:55 -0700

    chore: integrate vscode ext needed files for sample Pronunciation Assessment (#2842)
    * chore: integrate vscode ext needed files for sample AP
    
    * chore: remove available location link
    
    * docs: update readme
    
    * chore: remove duplicate json installation
    
    ---------
    
    Co-authored-by: Di Lin <dilin@microsoft.com>

--- scenarios/python/console/language-learning/pronunciation_assessment.py
+++ scenarios/python/console/language-learning/pronunciation_assessment.py
@@ -19,10 +19,11 @@ import requests
 
 from utils import read_wave_header, push_stream_writer, get_reference_words, WaveHeader16K16BitMono
 
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
 
-# Set up the subscription info for the Speech Service:
-# Replace with your own subscription key and endpoint.
-speech_key, service_region = "YourSubscriptionKey", "YourServiceRegion"
+speech_key = config.get("SubscriptionKey")
+service_region = config.get("ServiceRegion")
 speech_endpoint = f"https://{service_region}.api.cognitive.microsoft.com"
 
 
