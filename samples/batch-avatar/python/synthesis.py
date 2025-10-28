334f2a5874720291e88d53545de1d09404c4cc8c
Zhifan Zhang <zhifzhan@microsoft.com>
2025-10-28 16:09:39 +0800

    Fix lint

--- samples/batch-avatar/python/synthesis.py
+++ samples/batch-avatar/python/synthesis.py
@@ -90,7 +90,7 @@ def submit_synthesis(job_id: str):
             "subtitleType": "soft_embedded",
             "backgroundColor": "#FFFFFFFF",  # background color in RGBA format, default is white;
                                                # can be set to 'transparent' for transparent background
-            # "backgroundImage": "https://samples-files.com/samples/Images/jpg/1920-1080-sample.jpg", 
+            # "backgroundImage": "https://samples-files.com/samples/Images/jpg/1920-1080-sample.jpg",
             # background image URL, only support https, either backgroundImage or backgroundColor can be set
             "useBuiltInVoice": False,  # whether to use voice sync of custom avatar.
                                        # Only valid if customized is True and the custom avatar is trained with voice sync enabled.
