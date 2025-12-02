34d363412a43290da999f6357b2b48d8f27e3222
rohith-nagamalla <nagamalla.rohith@gmail.com>
2025-12-02 17:02:43 +0530

    added comments back

--- samples/cpp/intent-recognition/samples/intent_api/intentapi_cxx_intent_recognition_result.h
+++ samples/cpp/intent-recognition/samples/intent_api/intentapi_cxx_intent_recognition_result.h
@@ -24,16 +24,37 @@ class INTENT_API IntentRecognitionResult final
 {
 public:
 
+    /// <summary>
+    /// Constructor. Creates a new instance using the provided parameters.
+    /// </summary>
+    /// <param name="intentId">Result intent id.</param>
+    /// <param name="jsonResult">Simple result JSON string.</param>
+    /// <param name="detailedJsonResult">Detailed result JSON string.</param>
     IntentRecognitionResult(std::string& intentId, std::string& jsonResult, std::string& detailedJsonResult);
     IntentRecognitionResult(const IntentRecognitionResult& other);
     IntentRecognitionResult& operator=(const IntentRecognitionResult& other);
     IntentRecognitionResult(IntentRecognitionResult&&) = delete;
     IntentRecognitionResult& operator=(IntentRecognitionResult&&) = delete;
 
+    /// <summary>
+    /// A call to return a map of the entities found in the utterance.
+    /// </summary>
+    /// <returns>
+    /// A map with the entity name as a key and containing the value of the entity found in the utterance.
+    /// </returns>
     const std::map<std::string, std::string>& GetEntities() const;
 
+    /// <summary>
+    /// A call to return the detailed results.
+    /// </summary>
+    /// <returns>
+    /// The detailed results as a JSON string.
+    /// </returns>
     const std::string& GetDetailedResult() const;
 
+    /// <summary>
+    /// Destructor.
+    /// </summary>
     ~IntentRecognitionResult();
 
 private:
--- samples/cpp/intent-recognition/samples/intent_api/intentapi_cxx_intent_recognizer.h
+++ samples/cpp/intent-recognition/samples/intent_api/intentapi_cxx_intent_recognizer.h
@@ -31,8 +31,18 @@ class INTENT_API IntentRecognizer
 {
 public:
 
+    /// <summary>
+    /// Creates an intent recognizer from the specified language.
+    /// Users should use this function to create a new instance of an intent recognizer.
+    /// </summary>
+    /// <param name="language">Language tag in BCP 47 format.</param>
+    /// <returns>Instance of intent recognizer.</returns>
     static std::shared_ptr<IntentRecognizer> FromLanguage(const std::string& language = "en-US");
 
+    /// <summary>
+    /// Internal constructor. Creates a new instance from the provided language.
+    /// </summary>
+    /// <param name="language">Language tag in BCP 47 format.</param>
     explicit IntentRecognizer(const std::string& language);
 
     IntentRecognizer(const IntentRecognizer&) = delete;
@@ -40,22 +50,81 @@ public:
     IntentRecognizer(IntentRecognizer&&) = delete;
     IntentRecognizer& operator=(IntentRecognizer&&) = delete;
 
+    /// <summary>
+    /// Destructor.
+    /// </summary>
     ~IntentRecognizer();
 
+    /// <summary>
+    /// Starts intent recognition, and generates a result from the text passed in.
+    /// It is only valid for offline pattern matching or exact matching intents.
+    /// </summary>
+    /// <param name="text">The text to be evaluated.</param>
+    /// <returns>Future containing result value (a shared pointer to IntentRecognitionResult)
+    /// of the asynchronous intent recognition.
+    /// </returns>
     std::future<std::shared_ptr<IntentRecognitionResult>> RecognizeOnceAsync(std::string text);
 
+    /// <summary>
+    /// Adds a simple phrase that may appear in the input text, indicating a specific user intent.
+    /// This simple phrase can be a pattern including and enitity surrounded by braces. Such as "click the {checkboxName} checkbox".
+    /// 
+    /// </summary>
+    /// <param name="simplePhrase">The phrase corresponding to the intent.</param>
+    /// <remarks>Once recognized, the IntentRecognitionResult's IntentId property will match the simplePhrase specified here.
+    /// If any entities are specified and matched, they will be available in the IntentResult->GetEntities() call.
+    /// </remarks>
     void AddIntent(const std::string& simplePhrase);
 
+    /// <summary>
+    /// Adds a simple phrase that may appear in the input text, indicating a specific user intent.
+    /// This simple phrase can be a pattern including and enitity surrounded by braces. Such as "click the {checkboxName} checkbox".
+    /// </summary>
+    /// <param name="simplePhrase">The phrase corresponding to the intent.</param>
+    /// <param name="intentId">A custom id string to be returned in the IntentRecognitionResult's IntentId property.</param>
+    /// <remarks>Once recognized, the result's intent id will match the id supplied here.
+    /// If any entities are specified and matched, they will be available in the IntentResult->GetEntities() call.
+    /// </remarks>
     void AddIntent(const std::string& simplePhrase, const std::string& intentId);
 
+    /// <summary>
+    /// Adds an intent by name from the specified Language Understanding Model.
+    /// For PatternMatchingModel type, this will clear any existing models and add all intents from it. The intentName is ignored.
+    /// </summary>
+    /// <param name="model">The language understanding model containing the intent.</param>
+    /// <param name="intentName">The name of the single intent to be included from the language understanding model.</param>
     void AddIntent(std::shared_ptr<LanguageUnderstandingModel> model, const std::string& intentName);
 
+    /// <summary>
+    /// Adds an intent by name from the specified Language Understanding Model.
+    /// For PatternMatchingModel type, this will clear any existing models and add all intents from it. The intentName and intentId are ignored.
+    /// </summary>
+    /// <param name="model">The language understanding model containing the intent.</param>
+    /// <param name="intentName">The name of the single intent to be included from the language understanding model.</param>
+    /// <param name="intentId">A custom id string to be returned in the IntentRecognitionResult's IntentId property.</param>
     void AddIntent(std::shared_ptr<LanguageUnderstandingModel> model, const std::string& intentName, const std::string& intentId);
 
+    /// <summary>
+    /// Adds all intents from the specified Language Understanding Model.
+    /// For PatternMatchingModel type, this will clear any existing models before enabling it.
+    /// </summary>
+    /// <param name="model">The language understanding model containing the intents.</param>
     void AddAllIntents(std::shared_ptr<LanguageUnderstandingModel> model);
 
+    /// <summary>
+    /// Adds all intents from the specified Language Understanding Model.
+    /// For PatternMatchingModel type, this will clear any existing models before enabling it. The intentId is ignored.
+    /// </summary>
+    /// <param name="model">The language understanding model containing the intents.</param>
+    /// <param name="intentId">A custom string id to be returned in the IntentRecognitionResult's IntentId property.</param>
     void AddAllIntents(std::shared_ptr<LanguageUnderstandingModel> model, const std::string& intentId);
 
+    /// <summary>
+    /// Takes a collection of language understanding models, makes a copy of them, and applies them to the recognizer.
+    /// This replaces any previously applied models.
+    /// </summary>
+    /// <param name="collection">A vector of shared pointers to LanguageUnderstandingModels.</param>
+    /// <returns>True if the application of the models takes effect immediately. Otherwise false.</returns>
     bool ApplyLanguageModels(const std::vector<std::shared_ptr<LanguageUnderstandingModel>>& collection);
 
 private:
